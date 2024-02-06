b2c187bc338c384b8e28eca587f8d385553ba82f
seojeewon <readyjiwon@gmail.com>
2024-02-06 14:25:43 +0900

    [BE] feat: springboot configuration
    dependency 설정 및 mysql 연동

---
+++ backend/driving-today/src/main/java/com/drivingtoday/DrivingTodayApplication.java
@@ -0,0 +1,13 @@
+package com.drivingtoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingTodayApplication.class, args);
+	}
+
+}
---
+++ backend/driving-today/src/test/java/com/drivingtoday/DrivingTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivingtoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
