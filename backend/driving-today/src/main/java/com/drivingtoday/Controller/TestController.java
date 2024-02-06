2f388dff28ccba4be583d46d5155ef8aafe0e23e
admin <lejs1008@naver.com>
2024-02-06 18:46:37 +0900

    test

--- backend/driving-today/src/main/java/com/drivingtoday/Controller/TestController.java
+++ backend/driving-today/src/main/java/com/drivingtoday/Controller/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
     @Operation(summary = "swagger test")
     @GetMapping("/")
     public String swaggerTest(){
-
+        System.out.println("zz");
         return "test";
     }
 
