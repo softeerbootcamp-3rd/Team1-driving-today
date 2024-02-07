d410207f41abff9e1c4169656410f733b94afeb6
admin <lejs1008@naver.com>
2024-02-07 17:24:17 +0900

    [BE] feat : JPA Repository들 생성

---
+++ backend/driving-today/src/main/java/com/drivingtoday/repository/AcademyRepository.java
@@ -0,0 +1,7 @@
+package com.drivingtoday.repository;
+
+import com.drivingtoday.entity.Academy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcademyRepository extends JpaRepository<Academy, Long> {
+}
