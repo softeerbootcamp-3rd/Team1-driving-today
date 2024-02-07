f0505e2a32dc18544f40e6e36722007f878f73dd
admin <lejs1008@naver.com>
2024-02-07 11:42:48 +0900

    [BE] feat : 학원 entity 생성

--- backend/driving-today/src/main/java/com/drivingtoday/entity/Academy.java
+++ backend/driving-today/src/main/java/com/drivingtoday/entity/Academy.java
@@ -1,2 +1,29 @@
-package com.drivingtoday.entity;public class Academy {
+package com.drivingtoday.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Academy {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "academy_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "cert")
+    private Boolean cert;
+
+
 }
