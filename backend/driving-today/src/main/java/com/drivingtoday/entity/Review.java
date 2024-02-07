5055f96989f49ba59a9c038f4c98ae5b4878e961
admin <lejs1008@naver.com>
2024-02-07 12:03:37 +0900

    [BE] feat : 리뷰 entity 생성

--- backend/driving-today/src/main/java/com/drivingtoday/entity/Review.java
+++ backend/driving-today/src/main/java/com/drivingtoday/entity/Review.java
@@ -1,2 +1,42 @@
-package com.drivingtoday.entity;public class Review {
+package com.drivingtoday.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @Column(name = "contents")
+    private String contents;
+
+    @Column(name = "rating")
+    @NotNull
+    private Double rating;
+
+    @Column(name = "createdAt")
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    @NotNull
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id")
+    @NotNull
+    private Instructor instructor;
+
 }
