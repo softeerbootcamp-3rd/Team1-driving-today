2139030fef5c8e6ebd065dda09c08dd1c64eca39
admin <lejs1008@naver.com>
2024-02-08 11:57:18 +0900

    [BE] feat : InstructorService 계층 도입

--- backend/driving-today/src/main/java/com/drivingtoday/service/InstructorService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/service/InstructorService.java
@@ -1,2 +1,21 @@
-package com.drivingtoday.service;public class InstructorService {
+package com.drivingtoday.service;
+
+import com.drivingtoday.entity.Instructor;
+import com.drivingtoday.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class InstructorService {
+
+    private final ReservationRepository reservationRepository;
+
+    public List<Instructor> getInstructorList(LocalDate reservationDate, int reservationTime, int trainingTime) {
+        List<Instructor> availableInstructors = reservationRepository.findAvailableInstructors(reservationDate, reservationTime, trainingTime);
+        return availableInstructors;
+    }
 }
