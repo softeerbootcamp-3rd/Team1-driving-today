4f163585e8e412c8fc33db86176d33267cfaa72e
admin <lejs1008@naver.com>
2024-02-09 11:44:01 +0900

    [BE] feat : Response에 담을 강사 정보 클래스 정의

--- backend/driving-today/src/main/java/com/drivingtoday/dto/response/AvailableInstructor.java
+++ backend/driving-today/src/main/java/com/drivingtoday/dto/response/AvailableInstructor.java
@@ -1,5 +1,28 @@
 package com.drivingtoday.dto.response;
 
-public class AvailableInstructors {
-    
+import com.drivingtoday.entity.Instructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AvailableInstructor {
+    private Long instructorId;
+    private String instructorName;
+    private AcademyInfo academyInfo;
+    private Integer pricePerHour;
+    private String phoneNumber;
+    private String image;
+    private String introduction;
+
+    public static AvailableInstructor from(Instructor instructor) {
+        return AvailableInstructor.builder()
+                .instructorId(instructor.getId())
+                .instructorName(instructor.getName())
+                .academyInfo(AcademyInfo.from(instructor.getAcademy()))
+                .pricePerHour(instructor.getPricePerHour())
+                .image(instructor.getInstructorImage())
+                .introduction(instructor.getIntroduction())
+                .build();
+    }
 }
