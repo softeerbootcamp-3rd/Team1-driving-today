7706d158e0a0076b3af29a840f0187bea8e60bfa
admin <lejs1008@naver.com>
2024-02-09 11:23:29 +0900

    [BE] feat : 서비스 계층 페이징 기능 지원하도록 수정

--- backend/driving-today/src/main/java/com/drivingtoday/service/InstructorService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/service/InstructorService.java
@@ -1,21 +1,29 @@
 package com.drivingtoday.service;
 
+import com.drivingtoday.dto.request.AvailableInstructorsRequest;
 import com.drivingtoday.entity.Instructor;
-import com.drivingtoday.repository.ReservationRepository;
+import com.drivingtoday.repository.InstructorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class InstructorService {
 
-    private final ReservationRepository reservationRepository;
-
-    public List<Instructor> getInstructorList(LocalDate reservationDate, int reservationTime, int trainingTime) {
-        List<Instructor> availableInstructors = reservationRepository.findAvailableInstructors(reservationDate, reservationTime, trainingTime);
-        return availableInstructors;
+    private final InstructorRepository instructorRepository;
+    public List<Instructor> findAvailableInstructors(AvailableInstructorsRequest request) {
+        return instructorRepository.findAvailableInstructors(
+                request.getReservationDate().toString(),
+                request.getReservationTime(),
+                request.getTrainingTime(),
+                request.getLatitude(),
+                request.getLongitude(),
+                request.getPageSize(),
+                (request.getPageNumber() - 1) * request.getPageSize()
+        );
     }
+
 }
