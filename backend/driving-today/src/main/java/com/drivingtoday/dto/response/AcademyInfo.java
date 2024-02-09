8b4b46b3c1cab42fe5230be490523a6e9fdcfc0a
admin <lejs1008@naver.com>
2024-02-09 11:43:25 +0900

    [BE] feat : Response에 담을 학원 정보 클래스 생성

--- backend/driving-today/src/main/java/com/drivingtoday/dto/response/AcademyInfo.java
+++ backend/driving-today/src/main/java/com/drivingtoday/dto/response/AcademyInfo.java
@@ -1,2 +1,21 @@
-package com.drivingtoday.dto.response;public class AcademyInfo {
+package com.drivingtoday.dto.response;
+
+import com.drivingtoday.entity.Academy;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AcademyInfo {
+    private String academyName;
+    private Double latitude;
+    private Double longitude;
+
+    public static AcademyInfo from(Academy academy) {
+        return AcademyInfo.builder()
+                .academyName(academy.getName())
+                .latitude(academy.getLatitude())
+                .longitude(academy.getLongitude())
+                .build();
+    }
 }
