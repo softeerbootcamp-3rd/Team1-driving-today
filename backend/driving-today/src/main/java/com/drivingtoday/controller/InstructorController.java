f7a134e6c9bf31bcded618915c98dc72a6a24ba7
admin <lejs1008@naver.com>
2024-02-09 11:46:00 +0900

    [BE] feat : 반환받은 강사 list를 순회하면서 응답 형태로 변환하는 controller 작성

--- backend/driving-today/src/main/java/com/drivingtoday/controller/InstructorController.java
+++ backend/driving-today/src/main/java/com/drivingtoday/controller/InstructorController.java
@@ -1,2 +1,33 @@
-package com.drivingtoday.controller;public class InstructorController {
+package com.drivingtoday.controller;
+
+import com.drivingtoday.dto.request.AvailableInstructorsRequest;
+import com.drivingtoday.dto.response.AvailableInstructor;
+import com.drivingtoday.entity.Instructor;
+import com.drivingtoday.service.InstructorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class InstructorController {
+
+    private final InstructorService instructorService;
+
+    @GetMapping("/instructors")
+    public ResponseEntity<List<AvailableInstructor>> getInstructors(@ModelAttribute AvailableInstructorsRequest request) {
+
+        List<Instructor> instructorList = instructorService.findAvailableInstructors(request);
+
+        List<AvailableInstructor> availableInstructors = instructorList.stream()
+                .map(AvailableInstructor::from)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(availableInstructors);
+    }
 }
