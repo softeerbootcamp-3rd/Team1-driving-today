420a2834e83ad0feaea772b8dcced84d8344de69
zensen6 <junhee1469@gmail.com>
2024-02-10 22:34:35 +0900

    [BE] feat : reservation 모두 조회하는 service 작성

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationListService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationListService.java
@@ -1,2 +1,17 @@
-package com.drivingtoday.domain.reservation;public class ReservationListService {
+package com.drivingtoday.domain.reservation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationListService {
+
+    private final ReservationRepository reservationRepository;
+
+    public List<Reservation> findAllReservation(Long studentId){
+        return reservationRepository.findAllByStudentId(studentId);
+    }
 }
