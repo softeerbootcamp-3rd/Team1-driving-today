3ff9201a31c9523a85fb1691bbce81bf5db3b8c4
admin <lejs1008@naver.com>
2024-02-10 13:18:52 +0900

    [BE] test : InstructorService 기본 조회 테스트 추가

--- backend/driving-today/src/test/java/com/drivingtoday/service/InstructorServiceTest.java
+++ backend/driving-today/src/test/java/com/drivingtoday/service/InstructorServiceTest.java
@@ -1,4 +1,35 @@
+package com.drivingtoday.service;
+
+import com.drivingtoday.dto.request.AvailableInstructorsRequest;
+import com.drivingtoday.entity.Instructor;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
 class InstructorServiceTest {
-  
+
+    @Autowired
+    private InstructorService instructorService;
+    @Test
+    @DisplayName("기본 조회 테스트")
+    public void 기본_조회() {
+        AvailableInstructorsRequest request = new AvailableInstructorsRequest(
+                37.02, -122.13, 2, 9,
+                LocalDate.of(2024, 2, 8), 1, 10);
+
+        List<Instructor> availableInstructors = instructorService.findAvailableInstructors(request);
+        assertThat(availableInstructors.size()).isEqualTo(10);
+    }
+
 }
