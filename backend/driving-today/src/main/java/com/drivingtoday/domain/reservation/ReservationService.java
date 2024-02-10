bd4e767703d5f055cdc193455b040b131c4c8db9
zensen6 <junhee1469@gmail.com>
2024-02-10 16:50:14 +0900

    [BE] refactor : reservation service delete constructor and add annotation

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationService.java
@@ -6,6 +6,7 @@ import com.drivingtoday.domain.instructor.Instructor;
 import com.drivingtoday.domain.instructor.InstructorRepository;
 import com.drivingtoday.domain.student.Student;
 import com.drivingtoday.domain.student.StudentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.util.Optional;
 
 
 @Service
+@RequiredArgsConstructor
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
@@ -22,16 +24,7 @@ public class ReservationService {
 
     private final InstructorRepository instructorRepository;
 
-    public ReservationService(ReservationRepository reservationRepository, StudentRepository studentRepository, InstructorRepository instructorRepository){
-        this.reservationRepository = reservationRepository;
-        this.studentRepository = studentRepository;
-        this.instructorRepository = instructorRepository;
-    }
-
     public void saveReservations(ReservationRequest reservationRequest){
-
-        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        //LocalDate date = LocalDate.parse(reservationRequest.getReservationDate(), formatter);
 
         Optional<Student> optionalStudent = studentRepository.findById(1L);
         Student student = optionalStudent.orElseThrow(() -> new RuntimeException("Student is not present"));
