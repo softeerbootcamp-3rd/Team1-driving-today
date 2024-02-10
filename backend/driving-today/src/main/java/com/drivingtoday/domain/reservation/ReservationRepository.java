72ed5c2143c6a3f1a533493d7683f8cf84b84e79
zensen6 <junhee1469@gmail.com>
2024-02-10 22:32:18 +0900

    [BE] feat : 모든 reservation 조회하는 jpql 작성

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationRepository.java
@@ -2,6 +2,13 @@ package com.drivingtoday.domain.reservation;
 
 import com.drivingtoday.domain.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Query(value = "Select * from reservation Where student_id = ?1", nativeQuery = true)
+    List<Reservation> findAllByStudentId(Long studentId);
+
 }
