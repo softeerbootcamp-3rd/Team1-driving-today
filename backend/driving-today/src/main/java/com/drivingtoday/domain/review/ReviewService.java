ba389d74037f0d2512aefe7d27ffffa38fba2106
seojeewon <readyjiwon@gmail.com>
2024-02-10 17:19:01 +0900

    [BE] feat : ReviewService 생성
    리뷰 등록 메서드 추가

---
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewService.java
@@ -0,0 +1,31 @@
+package com.drivingtoday.domain.review;
+
+import com.drivingtoday.domain.review.ReviewRequest;
+import com.drivingtoday.domain.reservation.Reservation;
+import com.drivingtoday.domain.review.Review;
+import com.drivingtoday.domain.reservation.ReservationRepository;
+import com.drivingtoday.domain.review.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final ReservationRepository reservationRepository;
+
+    public Long addReview(ReviewRequest reviewRequest) {
+        Reservation targetReservation = reservationRepository.findById(reviewRequest.getReservationId())
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 예약입니다."));
+
+        Review review = Review.builder()
+                .rating(reviewRequest.getRating())
+                .contents(reviewRequest.getContents())
+                .student(targetReservation.getStudent())
+                .instructor(targetReservation.getInstructor())
+                .build();
+
+        reviewRepository.save(review);
+        return review.getId();
+    }
+}
