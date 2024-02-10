b9007540ef6cd1d876f2ee9ae441ff1ab9e2f456
seojeewon <readyjiwon@gmail.com>
2024-02-10 14:55:00 +0900

    [BE] feat: Review 저장 요청 DTO 새엇ㅇ

---
+++ backend/driving-today/src/main/java/com/drivingtoday/dto/ReviewRequest.java
@@ -0,0 +1,17 @@
+package com.drivingtoday.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReviewRequest {
+    @NotNull
+    private Double rating;
+
+    private String contents;
+
+    @NotNull
+    private Long reservationId;
+}
