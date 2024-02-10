9785cfba33687f1985e04fdcb3f01663f805412d
jsLeen <lejs1008@naver.com>
2024-02-10 23:48:12 +0900

    [BE] chore : 테스트 코드 import문 추가

--- backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewRepository.java
@@ -1,10 +1,7 @@
 package com.drivingtoday.domain.review;
 
-<<<<<<< HEAD
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-=======
->>>>>>> 2b4c3bd ([BE] chore : 불필요한 import문 및 setter 제거)
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
