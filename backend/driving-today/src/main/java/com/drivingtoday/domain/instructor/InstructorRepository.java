5b1e9fdfef4d63cf69264c9af1bdf94987378d66
seojeewon <readyjiwon@gmail.com>
2024-02-11 23:29:33 +0900

    [BE] chore : 불필요한 import문 제거

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorRepository.java
@@ -1,6 +1,5 @@
 package com.drivingtoday.domain.instructor;
 
-import com.drivingtoday.domain.instructor.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
