a6468ad45058a118630079d36a18f8534628e60f
seojeewon <readyjiwon@gmail.com>
2024-02-11 20:15:24 +0900

    [BE] feat : 회원가입 처리하는 서비스 작성

---
+++ backend/driving-today/src/main/java/com/drivingtoday/global/user/UserJoinService.java
@@ -0,0 +1,47 @@
+package com.drivingtoday.global.user;
+
+import com.drivingtoday.domain.student.Student;
+import com.drivingtoday.domain.student.StudentRepository;
+import com.drivingtoday.global.s3.S3UploadService;
+import com.drivingtoday.global.user.dto.StudentJoinRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class UserJoinService {
+    private final S3UploadService s3UploadService;
+    private final StudentRepository studentRepository;
+
+    public Long addStudent(StudentJoinRequest joinRequest, MultipartFile profileImg) {
+
+        //이미 존재하는 닉네임일 경우 예외 발생
+        if (studentRepository.findByNickname(joinRequest.getNickname()).isPresent()) {
+            throw new RuntimeException("이미 존재하는 닉네임입니다.");
+        }
+
+        //프로필 이미지 업로드
+        String imgUrl = "";
+        try {
+            if (profileImg != null) {
+                imgUrl = s3UploadService.uploadFile(profileImg, "profile/");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("S3 upload Fail");
+        }
+
+        //데이터베이스에 학생 저장
+        Student newStudent = Student.builder()
+                .name(joinRequest.getName())
+                .nickname(joinRequest.getNickname())
+                .phoneNumber(joinRequest.getPhoneNumber())
+                .studentImage(imgUrl)
+                .build();
+        studentRepository.save(newStudent);
+        return newStudent.getId();
+    }
+
+}
