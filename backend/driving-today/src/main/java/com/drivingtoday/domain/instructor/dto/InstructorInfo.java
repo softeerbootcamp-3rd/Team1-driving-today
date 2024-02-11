0d51d2733585f5f58633906132b8d1ca3aa5a60e
jsLeen <lejs1008@naver.com>
2024-02-11 18:47:34 +0900

    [BE] feat : 페이지에서 필요로 하는 데이터만 넘겨줄 DTO 추가

---
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/InstructorInfo.java
@@ -0,0 +1,25 @@
+package com.drivingtoday.domain.instructor.dto;
+
+import com.drivingtoday.domain.instructor.Instructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class InstructorInfo {
+    private String name;
+    private String phoneNumber;
+    private String instructorImage;
+    private Integer pricePerHour;
+    private String introduction;
+
+    public static InstructorInfo from(Instructor instructor) {
+        return InstructorInfo.builder()
+                .name(instructor.getName())
+                .phoneNumber(instructor.getPhoneNumber())
+                .instructorImage(instructor.getInstructorImage())
+                .pricePerHour(instructor.getPricePerHour())
+                .introduction(instructor.getIntroduction())
+                .build();
+    }
+}
---
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/ReviewDetail.java
@@ -0,0 +1,23 @@
+package com.drivingtoday.domain.instructor.dto;
+
+import com.drivingtoday.domain.review.Review;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ReviewDetail {
+    private String contents;
+    private Double rating;
+    private LocalDateTime createdAt;
+
+    public static ReviewDetail from(Review review) {
+        return ReviewDetail.builder()
+                .contents(review.getContents())
+                .rating(review.getRating())
+                .createdAt(review.getCreatedAt())
+                .build();
+    }
+}
