25b3762d8768650923a52bbbb510fb65a4c14a3f
zensen6 <junhee1469@gmail.com>
2024-02-11 22:38:08 +0900

    [BE] refactor : academy builder 형식으로 refactor

--- backend/driving-today/src/main/java/com/drivingtoday/domain/academy/Academy.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/academy/Academy.java
@@ -8,8 +8,6 @@ import lombok.*;
 @Entity
 @NoArgsConstructor
 @Getter
-@Builder
-@AllArgsConstructor
 public class Academy {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "academy_id")
@@ -30,4 +28,12 @@ public class Academy {
     @Column(name = "cert")
     @NotNull
     private Boolean cert;
+
+    @Builder
+    private Academy(String name, Double latitude, Double longitude, Boolean cert){
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.cert = cert;
+    }
 }
