4562ae4876eca2206fc91650732e80d27e06478c
seojeewon <readyjiwon@gmail.com>
2024-02-11 20:57:30 +0900

    [BE] feat : 리뷰 저장 메서드에 @Transactional 추가

--- backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewAddService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewAddService.java
@@ -5,6 +5,7 @@ import com.drivingtoday.domain.reservation.ReservationRepository;
 import com.drivingtoday.domain.review.dto.ReviewRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +13,7 @@ public class ReviewAddService {
     private final ReviewRepository reviewRepository;
     private final ReservationRepository reservationRepository;
 
+    @Transactional
     public Long addReview(ReviewRequest reviewRequest) {
         Reservation targetReservation = reservationRepository.findById(reviewRequest.getReservationId())
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 예약입니다."));
