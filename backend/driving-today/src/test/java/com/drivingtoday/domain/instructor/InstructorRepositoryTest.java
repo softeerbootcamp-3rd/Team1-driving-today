4d4d83bf42a86e72c70dfc5c09179275efdfd373
jsLeen <lejs1008@naver.com>
2024-02-11 22:41:58 +0900

    [BE] test : 페이징 방식 변경에 따른 테스트 코드 변경

--- backend/driving-today/src/test/java/com/drivingtoday/domain/instructor/InstructorRepositoryTest.java
+++ backend/driving-today/src/test/java/com/drivingtoday/domain/instructor/InstructorRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,7 +21,7 @@ class InstructorRepositoryTest {
     @DisplayName("기본 조회 테스트")
     public void test() {
         List<Instructor> availableInstructors = instructorRepository.findAvailableInstructors(
-                "2024-02-08", 9, 2, 37.02, -122.13, 10, 0
+                "2024-02-08", 9, 2, 37.02, -122.13, PageRequest.of(0, 10)
         );
         assertThat(availableInstructors.size()).isEqualTo(10);
     }
