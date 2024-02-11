ad0d940ecf48bb4616aca99724382e8b3528f151
jsLeen <lejs1008@naver.com>
2024-02-11 18:45:11 +0900

    [BE] feat : ReviewRepository에 강사id와 page로 조회 기능 추가

--- backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewRepository.java
@@ -1,7 +1,12 @@
 package com.drivingtoday.domain.review;
 
 import com.drivingtoday.domain.review.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findAllByInstructorId(Long instructorId, Pageable pageable);
 }
+
