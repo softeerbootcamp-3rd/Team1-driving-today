d0b847814a1e5cfb936377bfc4635bda6fafb08f
seojeewon <readyjiwon@gmail.com>
2024-02-11 09:57:44 +0900

    [BE] feat : ReviewRequest dto @AllArgsConstructor 추가

--- backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewRequest.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewRequest.java
@@ -1,12 +1,15 @@
 package com.drivingtoday.domain.review;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReviewRequest {
+
     @NotNull
     private Double rating;
 
