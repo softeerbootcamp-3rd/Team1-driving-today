d8a13300b519fa038a5abf293547613aa1e04822
seojeewon <readyjiwon@gmail.com>
2024-02-11 22:56:09 +0900

    [BE] chore : 불필요한 애너테이션 제거
    @AllArgsConstructor 제거

--- backend/driving-today/src/main/java/com/drivingtoday/global/user/dto/StudentJoinRequest.java
+++ backend/driving-today/src/main/java/com/drivingtoday/global/user/dto/StudentJoinRequest.java
@@ -4,9 +4,10 @@ import com.drivingtoday.domain.student.Student;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class StudentJoinRequest {
     @NotNull
     private String name;
