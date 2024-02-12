657c8e229a719a0e441af9271f4bea2440375f1f
seojeewon <readyjiwon@gmail.com>
2024-02-12 20:40:38 +0900

    [BE] feat : InstructorRepository 이메일로 Instructor 찾는 메서드 추가

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorRepository.java
@@ -2,5 +2,8 @@ package com.drivingtoday.domain.instructor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
+    Optional<Instructor> findByEmail(String email);
 }
