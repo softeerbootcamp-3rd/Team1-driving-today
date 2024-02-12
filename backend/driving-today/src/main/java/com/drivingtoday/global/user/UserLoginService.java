92de89ba92055db821c7bb8b46bf7994534ad45c
seojeewon <readyjiwon@gmail.com>
2024-02-12 20:41:06 +0900

    [BE] feat : 강사 로그인 서비스 구현

--- backend/driving-today/src/main/java/com/drivingtoday/global/user/UserLoginService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/global/user/UserLoginService.java
@@ -1,5 +1,6 @@
 package com.drivingtoday.global.user;
 
+import com.drivingtoday.domain.instructor.Instructor;
 import com.drivingtoday.domain.instructor.InstructorRepository;
 import com.drivingtoday.domain.student.Student;
 import com.drivingtoday.domain.student.StudentRepository;
@@ -24,5 +25,17 @@ public class UserLoginService {
             throw new RuntimeException("비밀번호가 틀렸습니다.");
         }
         return LoginResponse.of(student.getId());
+    }
+
+    public LoginResponse loginInstructor(LoginRequest request) {
+        //1. 유효한 이메일인지 확인
+        Instructor instructor = instructorRepository.findByEmail(request.getEmail()).orElseThrow(
+                () -> new RuntimeException("존재하지 않는 이메일입니다.")
+        );
+        //2. 패스워드 확인
+        if (!instructor.getPassword().equals(request.getPassword())) {
+            throw new RuntimeException("비밀번호가 틀렸습니다.");
+        }
+        return LoginResponse.of(instructor.getId());
     }
 }
