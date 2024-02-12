d1f560b4758d52ffd8e6f0cd267675f0d45c9688
zensen6 <junhee1469@gmail.com>
2024-02-12 22:00:31 +0900

    [BE] feat : lazy_loading 작성에 따른 서비스 계층 변화

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationListService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationListService.java
@@ -1,11 +1,15 @@
 package com.drivingtoday.domain.reservation;
 
+import com.drivingtoday.domain.instructor.dto.InstructorDTO;
 import com.drivingtoday.domain.reservation.dto.ReservationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,18 +22,22 @@ public class ReservationListService {
         List<Reservation> reservationList = reservationRepository.findAllByStudentId(studentId);
         List<ReservationResponse> reservationResponses = new ArrayList<>();
 
-        for(Reservation reservation : reservationList){
-            reservationResponses.add(ReservationResponse.builder()
-                            .id(reservation.getId())
-                            .reservationTime(reservation.getReservationTime())
-                            .reservationDate(reservation.getReservationDate())
-                            .isAccepted(reservation.getIsAccepted())
-                            .trainingTime(reservation.getTrainingTime())
-                            .createdAt(reservation.getCreatedAt())
-                            .instructor(reservation.getInstructor())
-                    .build());
-        }
+        reservationList.forEach(reservation -> {
+            InstructorDTO instructorDTO = InstructorDTO.convertToDTO(reservation.getInstructor());
 
+            reservationResponses.add(ReservationResponse.builder()
+                    .id(reservation.getId())
+                    .reservationTime(reservation.getReservationTime())
+                    .reservationDate(reservation.getReservationDate())
+                    .isAccepted(reservation.getIsAccepted())
+                    .trainingTime(reservation.getTrainingTime())
+                    .createdAt(reservation.getCreatedAt())
+                    .instructorDTO(instructorDTO)
+                    .build());
+        });
+
+        reservationResponses.sort(Comparator.comparing(ReservationResponse::getCreatedAt).reversed());
         return reservationResponses;
+
     }
 }
