faff094aca4e7ee61d51238086f4353a26fa6385
zensen6 <junhee1469@gmail.com>
2024-02-12 21:59:05 +0900

    [BE] feat : lazy_loading 을 작동시키기 위한 DTO 작성

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/dto/ReservationDTO.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/dto/ReservationDTO.java
@@ -1,2 +1,34 @@
-package com.drivingtoday.domain.reservation.dto;public class ReservationDTO {
+package com.drivingtoday.domain.reservation.dto;
+
+import com.drivingtoday.domain.reservation.Reservation;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ReservationDTO {
+    private Long id;
+    private Boolean isAccepted;
+    private LocalDateTime createdAt;
+    private LocalDate reservationDate;
+    private Integer reservationTime;
+    private Integer trainingTime;
+    private Long studentId;
+    private Long instructorId;
+
+    public static ReservationDTO convertToDTO(Reservation reservation){
+        ReservationDTO reservationDTO = new ReservationDTO();
+        reservationDTO.setId(reservation.getId());
+        reservationDTO.setIsAccepted(reservation.getIsAccepted());
+        reservationDTO.setCreatedAt(reservation.getCreatedAt());
+        reservationDTO.setReservationDate(reservation.getReservationDate());
+        reservationDTO.setReservationTime(reservation.getReservationTime());
+        reservationDTO.setTrainingTime(reservation.getTrainingTime());
+        reservationDTO.setStudentId(reservation.getStudent().getId());
+        reservationDTO.setInstructorId(reservation.getInstructor().getId());
+        return reservationDTO;
+    }
 }
