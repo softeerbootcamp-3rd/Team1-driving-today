d0627ab28ffd880c7a1bbc45f3f30322c44cb7ea
zensen6 <junhee1469@gmail.com>
2024-02-12 21:57:48 +0900

    [BE] feat : lazy_loading 을 작동시키기 위한 DTO 작성

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/InstructorDTO.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/InstructorDTO.java
@@ -1,2 +1,31 @@
-package com.drivingtoday.domain.instructor.dto;public class InstructorDTO {
+package com.drivingtoday.domain.instructor.dto;
+
+import com.drivingtoday.domain.academy.Academy;
+import com.drivingtoday.domain.academy.dto.AcademyDTO;
+import com.drivingtoday.domain.instructor.Instructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InstructorDTO {
+
+    private String name;
+    private String phoneNumber;
+    private String instructorImage;
+    private Integer pricePerHour;
+    private String introduction;
+    private AcademyDTO academyDTO;
+
+    public static InstructorDTO convertToDTO(Instructor instructor){
+        InstructorDTO instructorDTO = new InstructorDTO();
+        instructorDTO.setName(instructor.getName());
+        instructorDTO.setPhoneNumber(instructor.getPhoneNumber());
+        instructorDTO.setInstructorImage(instructor.getInstructorImage());
+        instructorDTO.setPricePerHour(instructor.getPricePerHour());
+        instructorDTO.setIntroduction(instructor.getIntroduction());
+        instructorDTO.setAcademyDTO(AcademyDTO.convertToDTO(instructor.getAcademy()));
+        return instructorDTO;
+    }
+
 }
