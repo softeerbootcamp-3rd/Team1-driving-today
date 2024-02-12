41dd33bc3959dd347d8ef4bcbeecad7b258ee684
jsLeen <lejs1008@naver.com>
2024-02-12 11:02:49 +0900

    [BE] feat : 변환한 응답형태에 따른 db 쿼리 변경

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorRepository.java
@@ -1,5 +1,6 @@
 package com.drivingtoday.domain.instructor;
 
+import com.drivingtoday.domain.instructor.dto.AvailableInstructorInfo;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,7 +9,9 @@ import java.util.List;
 
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
 
-    @Query(value = "SELECT i.* " +
+    @Query(value = "SELECT i.instructor_id as instructorId, i.name as instructorName, i.instructor_image as instructorImage, i.price_per_hour as pricePerHour, " +
+            "a.name as academyName, a.latitude as latitude, a.longitude as longitude, a.cert as cert, " +
+            "ROUND(ST_DISTANCE_SPHERE(POINT(a.longitude, a.latitude), POINT(:longitude, :latitude)) / 1000, 2) as distance " +
             "FROM Instructor i " +
             "JOIN Academy a ON i.academy_id = a.academy_id " +
             "WHERE NOT EXISTS " +
@@ -16,9 +19,9 @@ public interface InstructorRepository extends JpaRepository<Instructor, Long> {
             "WHERE r.instructor_id = i.instructor_id " +
             "AND r.reservation_date = :reservationDate " +
             "AND r.reservation_time BETWEEN :reservationTime AND :reservationTime + :trainingTime - 1) " +
-            "ORDER BY ST_DISTANCE_SPHERE(POINT(a.longitude, a.latitude), POINT(:longitude, :latitude)) ",
+            "ORDER BY distance",
             nativeQuery = true)
-    List<Instructor> findAvailableInstructors(
+    List<AvailableInstructorInfo> findAvailableInstructors(
             String reservationDate,
             int reservationTime,
             int trainingTime,
