87e77f3155c11b546f928908ea209761ee5b618e
zensen6 <junhee1469@gmail.com>
2024-02-12 22:03:05 +0900

    [BE] feat : reservation delete API 작성

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationController.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationController.java
@@ -1,17 +1,16 @@
 package com.drivingtoday.domain.reservation;
 
 
+import com.drivingtoday.domain.reservation.dto.ReservationDTO;
 import com.drivingtoday.domain.reservation.dto.ReservationRequest;
 import com.drivingtoday.domain.reservation.dto.ReservationResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
@@ -24,12 +23,14 @@ public class ReservationController {
 
     private final ReservationListService reservationListService;
 
+    private final ReservationDeleteService reservationDeleteService;
+
     @PostMapping("/reservations")
     @Operation(summary = "예약만들기 API")
     public ResponseEntity<Void> reservationAdd(@RequestBody ReservationRequest reservationRequest){
-        Reservation reservation = reservationAddService.addReservation(reservationRequest);
+        ReservationDTO reservationDTO = reservationAddService.addReservation(reservationRequest);
 
-        return ResponseEntity.created(URI.create("/reservation/" + reservation.getId())).build();
+        return ResponseEntity.created(URI.create("/reservation/" + reservationDTO.getId())).build();
     }
 
     @GetMapping("/my/reservations")
@@ -41,5 +42,11 @@ public class ReservationController {
         return ResponseEntity.ok().body(contents);
     }
 
+    @DeleteMapping("/reservations/{reservation_id}")
+    @Operation(summary = "예약 취소하기 API")
+    public ResponseEntity<String> deleteReservation(@PathVariable("reservation_id") @Parameter(description = "삭제할 예약의 ID", required = true) Long reservationId) {
+
+        return reservationDeleteService.deleteReservation(reservationId);
+    }
 
 }
--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationDeleteService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationDeleteService.java
@@ -1,2 +1,34 @@
-package com.drivingtoday.domain.reservation;public class ReservationDeleteService {
+package com.drivingtoday.domain.reservation;
+
+import com.drivingtoday.domain.instructor.Instructor;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.beans.Transient;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationDeleteService {
+
+    private final ReservationRepository reservationRepository;
+
+    @Transactional
+    public ResponseEntity<String> deleteReservation(Long reservationId){
+        try{
+            Reservation reservation = reservationRepository.findById(reservationId)
+                    .orElseThrow(() -> new IllegalArgumentException("reservation doesn't exists"));
+
+            Instructor instructor = reservation.getInstructor();
+
+            reservationRepository.delete(reservation);
+
+            return ResponseEntity.ok("Reservation deleted Successfully");
+        }catch(Exception ignored){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete reservation.");
+        }
+    }
+
 }
