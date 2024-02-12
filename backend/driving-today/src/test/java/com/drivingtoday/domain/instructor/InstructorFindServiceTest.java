b9b8be17f43f44fcb4238764be0d1350abc73873
jsLeen <lejs1008@naver.com>
2024-02-12 13:02:34 +0900

    [BE] test : 기존 리뷰와 관련된 테스트 코드 제거

--- backend/driving-today/src/test/java/com/drivingtoday/domain/instructor/InstructorFindServiceTest.java
+++ backend/driving-today/src/test/java/com/drivingtoday/domain/instructor/InstructorFindServiceTest.java
@@ -22,48 +22,9 @@ public class InstructorFindServiceTest {
     @DisplayName("존재하지 않는 강사id로 조회된 경우 예외 발생")
     public void 없는_강사_조회_테스트() {
         RuntimeException e = assertThrows(RuntimeException.class, () -> {
-            instructorFindService.findInstructor(13L, 1, 5);
+            instructorFindService.findInstructor(13L);
         });
         assertThat("해당 강사가 존재하지 않습니다.").isEqualTo(e.getMessage());
     }
 
-    @Test
-    @DisplayName("ID가 1인 강사의 리뷰를 조회하면 모든 리뷰(10개) 리스트가 반환된다")
-    public void 기본_리뷰_조회_테스트() {
-        InstructorDetailResponse response = instructorFindService.findInstructor(1L, 1, 10);
-        assertThat(response.getReviews().size()).isEqualTo(10);
-    }
-
-    @Test
-    @DisplayName("리뷰가 하나도 없는 강사를 조회할 경우 빈 리스트가 반환된다")
-    public void 리뷰없는_강사_조회_테스트() {
-        InstructorDetailResponse response = instructorFindService.findInstructor(2L, 1, 1);
-        assertThat(response.getReviews()).isEmpty();
-    }
-
-    @Test
-    @DisplayName("저장된 10개의 리뷰를 5개씩 2page로 나눠지는지 테스트")
-    public void 페이징_테스트1() {
-        InstructorDetailResponse response1 = instructorFindService.findInstructor(1L, 1, 5);
-        assertThat(response1.getReviews().size()).isEqualTo(5);
-
-        InstructorDetailResponse response2 = instructorFindService.findInstructor(1L, 2, 5);
-        assertThat(response2.getReviews().size()).isEqualTo(5);
-    }
-
-    @Test
-    @DisplayName("저장된 10개의 리뷰를 3개씩 3page + 1개 1page로 나눠지는지 테스트")
-    public void 페이징_테스트2() {
-        InstructorDetailResponse response1 = instructorFindService.findInstructor(1L, 1, 3);
-        assertThat(response1.getReviews().size()).isEqualTo(3);
-
-        InstructorDetailResponse response2 = instructorFindService.findInstructor(1L, 2, 3);
-        assertThat(response2.getReviews().size()).isEqualTo(3);
-
-        InstructorDetailResponse response3 = instructorFindService.findInstructor(1L, 3, 3);
-        assertThat(response3.getReviews().size()).isEqualTo(3);
-
-        InstructorDetailResponse response4 = instructorFindService.findInstructor(1L, 4, 3);
-        assertThat(response4.getReviews().size()).isEqualTo(1);
-    }
 }
