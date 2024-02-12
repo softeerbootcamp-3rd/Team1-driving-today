5fe901dd17e31c34da304e69c02c9209565c8b94
jsLeen <lejs1008@naver.com>
2024-02-12 11:01:41 +0900

    [BE] refactor : 정보 응답 형태 인터페이스로 변경

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/AvailableInstructorInfo.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/AvailableInstructorInfo.java
@@ -1,29 +1,13 @@
 package com.drivingtoday.domain.instructor.dto;
 
-import com.drivingtoday.domain.instructor.Instructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class AvailableInstructorInfo {
-    private Long instructorId;
-    private String instructorName;
-    private AcademyInfo academyInfo;
-    private Integer pricePerHour;
-    private String phoneNumber;
-    private String image;
-    private String introduction;
-
-    public static AvailableInstructorInfo from(Instructor instructor) {
-        return AvailableInstructorInfo.builder()
-                .instructorId(instructor.getId())
-                .instructorName(instructor.getName())
-                .academyInfo(AcademyInfo.from(instructor.getAcademy()))
-                .pricePerHour(instructor.getPricePerHour())
-                .phoneNumber(instructor.getPhoneNumber())
-                .image(instructor.getInstructorImage())
-                .introduction(instructor.getIntroduction())
-                .build();
-    }
+public interface AvailableInstructorInfo {
+    Long getInstructorId();
+    String getInstructorName();
+    String getInstructorImage();
+    Integer getPricePerHour();
+    String getAcademyName();
+    Double getLatitude();
+    Double getLongitude();
+    Boolean getCert();
+    Double getDistance();
 }
