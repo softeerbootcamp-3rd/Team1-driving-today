aad2e475ce0906e3849338d5271e5a92f4e44317
jsLeen <lejs1008@naver.com>
2024-02-13 18:31:07 +0900

    [BE] feat : 페이징 기능 추가로 인한 컨트롤러 계층 수정

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationController.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationController.java
@@ -27,34 +27,31 @@ public class ReservationController {
     @Operation(summary = "예약만들기 API")
     public ResponseEntity<Void> reservationAdd(@RequestBody ReservationRequest reservationRequest){
         ReservationDetailResponse reservationDetailResponse = reservationAddService.addReservation(reservationRequest);
-
         return ResponseEntity.created(URI.create("/reservation/" + reservationDetailResponse.getReservationInfo().getId())).build();
     }
 
     @GetMapping("/my/reservations")
     @Operation(summary = "나의 예약리스트 확인하기 API")
-    public ResponseEntity<List<ReservationDetailResponse>> reservationList(){
-
+    public ResponseEntity<List<ReservationDetailResponse>> reservationList(@RequestParam("pageNumber") Integer pageNumber,
+                                                                           @RequestParam("pageSize") Integer pageSize){
         Long StudentId = 1L;
-        List<ReservationDetailResponse> contents = reservationListService.findAllStudentReservation(StudentId);
+        List<ReservationDetailResponse> contents = reservationListService.findAllStudentReservation(StudentId, pageNumber, pageSize);
         return ResponseEntity.ok().body(contents);
     }
 
     @Operation(summary = "강사가 본인 예약리스트 확인하기 API")
     @GetMapping("/reservations")
-    public ResponseEntity<List<ReservationDetailResponse>> instructorReservationList(){
-
+    public ResponseEntity<List<ReservationDetailResponse>> instructorReservationList(@RequestParam("pageNumber") Integer pageNumber,
+                                                                                     @RequestParam("pageSize") Integer pageSize){
         Long InstructorId = 1L;
-        List<ReservationDetailResponse> contents = reservationListService.findAllInstructorReservation(InstructorId);
+        List<ReservationDetailResponse> contents = reservationListService.findAllInstructorReservation(InstructorId, pageNumber, pageSize);
         return ResponseEntity.ok().body(contents);
     }
 
     @DeleteMapping("/reservations/{reservation_id}")
     @Operation(summary = "예약 취소하기 API")
     public ResponseEntity<Void> deleteReservation(@PathVariable("reservation_id") @Parameter(description = "삭제할 예약의 ID", required = true) Long reservationId) {
-
         reservationDeleteService.deleteReservation(reservationId);
-
         return ResponseEntity.ok().build();
     }
 
