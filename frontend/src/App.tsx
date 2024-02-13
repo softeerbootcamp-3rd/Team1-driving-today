764ef3db00c7057433715c71f5110d421446a97e
Jeongjin Oh <rojay.developer@gmail.com>
2024-02-13 17:36:54 +0900

    wip: folder setting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,39 +2,66 @@ import {ReactNode} from 'react'
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  LoaderFunction,
+  Outlet,
   redirect,
   Route,
   RouterProvider,
   useRouteLoaderData,
 } from 'react-router-dom'
 
+import {Sidebar} from './components/sidebar'
+import {InstructorDashboard, StudentDashboard} from './pages/dashboard/page'
+import {InstructorHistory, StudentHistory} from './pages/history/page'
+import {StudentPurchase} from './pages/purchase/page'
+import {StudentSchedule} from './pages/schedule/page'
+
+const customLoader: LoaderFunction = ({request}) => {
+  // TODO: neet to authorize
+  const isAuth = true
+  const isStudent = true
+
+  if (!isAuth) {
+    return redirect('/login')
+  }
+  return {isStudent}
+}
+
 export function App() {
   return <RouterProvider router={router} fallbackElement={<p>Initial Load...</p>} />
+}
+
+function Layout() {
+  return (
+    <>
+      <Sidebar.Root>
+        <Sidebar.LinkList>
+          <Sidebar.Link icon="home" to="/dashboard" label="홈" />
+          <Sidebar.Link icon="history" to="/history" label="예약 내역" />
+          <Sidebar.Link icon="makeReservation" to="/schedule" label="연수 예약" />
+        </Sidebar.LinkList>
+        <Sidebar.Footer>
+          <Sidebar.ChatButton />
+        </Sidebar.Footer>
+      </Sidebar.Root>
+
+      <Outlet />
+    </>
+  )
 }
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route index path="/" element={<>landing page</>} />
-      <Route
-        id="root"
-        loader={({request}) => {
-          // TODO: neet to authorize
-          const isAuth = false
-
-          if (!isAuth) {
-            return redirect('/login')
-          }
-          return {isStudent: true}
-        }}
-        element={<>home/layout</>}
-      >
+      <Route path="/login" element={<>login</>} />
+      <Route id="root" loader={customLoader} element={<Layout />}>
         <Route
           path="/dashboard"
           element={
             <RequireAuth>
               {({isStudent}) => {
-                return isStudent ? <>dashboard/student</> : <>dashboard/instructor</>
+                return isStudent ? <StudentDashboard /> : <InstructorDashboard />
               }}
             </RequireAuth>
           }
@@ -44,7 +71,7 @@ const router = createBrowserRouter(
           element={
             <RequireAuth>
               {({isStudent}) => {
-                return isStudent ? <>history/student</> : <>history/instructor</>
+                return isStudent ? <StudentHistory /> : <InstructorHistory />
               }}
             </RequireAuth>
           }
@@ -54,7 +81,17 @@ const router = createBrowserRouter(
           element={
             <RequireAuth>
               {({isStudent}) => {
-                return isStudent ? <>schedule/student</> : <>schedule/instructor</>
+                return isStudent ?? <StudentSchedule />
+              }}
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/purchase"
+          element={
+            <RequireAuth>
+              {({isStudent}) => {
+                return isStudent ?? <StudentPurchase />
               }}
             </RequireAuth>
           }
---
+++ frontend/src/pages/page.tsx
@@ -0,0 +1,3 @@
+function LandingPage() {
+  return <></>
+}
