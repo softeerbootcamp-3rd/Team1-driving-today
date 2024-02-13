cd8d269a6efcd8080286337cbe7d2413a2eb89a7
zensen6 <junhee1469@gmail.com>
2024-02-13 17:11:37 +0900

    [BE] refactor : reservation dto를 형식 통일

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/InstructorDetailResponse.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/InstructorDetailResponse.java
@@ -1,2 +1,20 @@
-package com.drivingtoday.domain.instructor.dto;public class InstructorDetailResponse {
+package com.drivingtoday.domain.instructor.dto;
+
+import com.drivingtoday.domain.academy.dto.AcademyInfo;
+import com.drivingtoday.domain.instructor.Instructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class InstructorDetailResponse {
+    private InstructorInfo instructorInfo;
+    private AcademyInfo academyInfo;
+
+    public static InstructorDetailResponse from(Instructor instructor) {
+        return InstructorDetailResponse.builder()
+                .instructorInfo(InstructorInfo.from(instructor))
+                .academyInfo(AcademyInfo.from(instructor.getAcademy()))
+                .build();
+    }
 }
