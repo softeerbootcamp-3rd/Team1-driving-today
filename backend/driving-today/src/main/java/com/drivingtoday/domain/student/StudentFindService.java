e64e350d3013b4a3b9c11bd1ce4a37a8eb2705e7
jsLeen <lejs1008@naver.com>
2024-02-13 16:53:51 +0900

    [BE] feat : 학생 정보 조회 API 개발 (#84)

---
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/student/StudentFindService.java
@@ -0,0 +1,20 @@
+package com.drivingtoday.domain.student;
+
+import com.drivingtoday.domain.student.dto.StudentInfo;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentFindService {
+
+    private final StudentRepository studentRepository;
+
+    @Transactional
+    public StudentInfo findStudent(Long studentId) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new RuntimeException("해당 학생이 존재하지 않습니다."));
+        return StudentInfo.from(student);
+    }
+}
