79dee197abcdda58e385e852460abbdfc94bf35e
zensen6 <junhee1469@gmail.com>
2024-02-13 17:44:34 +0900

    [BE] feat : 강사가 본인 예약 페이지 리스트 보기

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationRepository.java
@@ -11,4 +11,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Query(value = "select * from Reservation where student_id = ?1", nativeQuery = true)
     List<Reservation> findAllByStudentId(Long studentId);
 
+    @Query(value = "select * from Reservation where instructor_id = ?1", nativeQuery = true)
+    List<Reservation> findAllByInstructorId(Long instructorId);
+
 }
