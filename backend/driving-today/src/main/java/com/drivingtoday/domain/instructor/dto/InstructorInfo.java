d8e2ea8fee448271b062c41f6ca4b66f334f36cc
zensen6 <junhee1469@gmail.com>
2024-02-13 17:12:00 +0900

    [BE] refactor : instructor dto를 형식 통일

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/InstructorInfo.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/dto/InstructorInfo.java
@@ -1,2 +1,27 @@
-package com.drivingtoday.domain.instructor;public class InstructorInfo {
+package com.drivingtoday.domain.instructor.dto;
+
+import com.drivingtoday.domain.instructor.Instructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class InstructorInfo {
+    private Long id;
+    private String name;
+    private String phoneNumber;
+    private String instructorImage;
+    private Integer pricePerHour;
+    private String introduction;
+
+    public static InstructorInfo from(Instructor instructor) {
+        return InstructorInfo.builder()
+                .id(instructor.getId())
+                .name(instructor.getName())
+                .phoneNumber(instructor.getPhoneNumber())
+                .instructorImage(instructor.getInstructorImage())
+                .pricePerHour(instructor.getPricePerHour())
+                .introduction(instructor.getIntroduction())
+                .build();
+    }
 }
