935c05130369894c45fa9fe59c5de16e104098b3
junhee Kim <junhee1469@gmail.com>
2024-02-13 17:31:59 +0900

    [BE] feat : 예약 취소 API 작성 (#69)

---
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationDeleteService.java
@@ -0,0 +1,29 @@
+package com.drivingtoday.domain.reservation;
+
+import com.drivingtoday.domain.instructor.Instructor;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.beans.Transient;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationDeleteService {
+
+    private final ReservationRepository reservationRepository;
+
+    @Transactional
+    public void deleteReservation(Long reservationId){
+
+        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new RuntimeException("reservation doesn't exists"));
+        Instructor instructor = reservation.getInstructor();
+
+        reservationRepository.delete(reservation);
+
+        return;
+    }
+
+}
