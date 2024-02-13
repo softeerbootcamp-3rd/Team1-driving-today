48f37c91325bcca1af44b9d86c01c34dc8177950
junhea <97426534+junhea@users.noreply.github.com>
2024-02-13 22:22:45 +0900

    feat: card component

---
+++ frontend/src/components/card/README.md
@@ -0,0 +1,80 @@
+# Button
+
+## description
+
+- 카드 컴포넌트
+
+## props
+
+### ReservationResultCard
+
+|          name          |         description          |  default  |
+| :--------------------: | :--------------------------: | :-------: |
+|    `instructorName`    |                              |           |
+|     `academyName`      |                              |           |
+|     `pricePerHour`     |                              |           |
+|        `image`         |       profile pic url        |           |
+|       `distance`       |        distance in km        |           |
+|       `duration`       |       duration by hour       |           |
+|        `rating`        |                              |           |
+| `onRequestReservation` |    show button if defined    | undefined |
+|      `onContact`       |    show button if defined    | undefined |
+|       `selected`       | highlight background if true |   false   |
+
+### StudentHistoryCard
+
+|       name       |         description          |  default  |
+| :--------------: | :--------------------------: | :-------: |
+| `instructorName` |                              |           |
+|  `academyName`   |                              |           |
+|     `image`      |       profile pic url        |           |
+|    `dateStr`     |    formatted date string     |           |
+|    `timeStr`     |    formatted time string     |           |
+| `onReviewClick`  |    show button if defined    | undefined |
+|    `selected`    | highlight background if true |   false   |
+
+### InstructorHistoryCard
+
+|       name       |          description          |  default  |
+| :--------------: | :---------------------------: | :-------: |
+| `instructorName` |                               |           |
+|     `image`      |        profile pic url        |           |
+|    `phoneStr`    | formatted phone number string |           |
+|    `dateStr`     |     formatted date string     |           |
+|    `timeStr`     |     formatted time string     |           |
+| `onRejectClick`  |    show button if defined     | undefined |
+|    `selected`    | highlight background if true  |   false   |
+
+## use case
+
+```tsx
+<ReservationResultCard
+  instructorName="강사 이름"
+  academyName="학원 이름"
+  pricePerHour={10000}
+  image=""
+  distance={2.5}
+  duration={1}
+  rating={4.5}
+  onContact={()=>startChat(instructorId)}
+  onRequestReservation={requestReservation}
+/>
+
+<StudentHistoryCard
+  instructorName="강사 이름"
+  academyName="학원 이름"
+  image=""
+  dateStr="2024.03.01"
+  timeStr="12:00~13:00 (1시간)"
+  onReviewClick={showReviewForm}
+/>
+
+<InstructorHistoryCard
+  studentName="학생 이름"
+  phoneStr="010-0000-0000"
+  dateStr="2024.03.01"
+  timeStr="12:00~13:00 (1시간)"
+  image=""
+  onRejectClick={rejectReservation}
+/>
+```
---
+++ frontend/src/components/card/card.tsx
@@ -0,0 +1,237 @@
+import {Theme} from '@emotion/react'
+import styled from '@emotion/styled'
+import {HTMLAttributes, PropsWithChildren} from 'react'
+
+import {Button} from '../button'
+import {Icon, IconName} from '../icon'
+
+interface BaseContainerProps {
+  selected?: boolean
+}
+
+const BaseContainer = styled.div<BaseContainerProps>(({theme, selected}) => ({
+  display: 'flex',
+  padding: '2rem',
+  gap: '2rem',
+  borderRadius: '1.6rem',
+  alignItems: 'center',
+  backgroundColor: selected ? theme.color.gray100 : theme.color.white,
+  border: `1px solid ${theme.color.gray200}`,
+}))
+
+const ContentContainer = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.5rem',
+  flexGrow: 1,
+})
+
+const ActionsContainer = styled.div({
+  display: 'flex',
+  paddingTop: '1rem',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  gap: '0.5rem',
+  '& > button': {
+    width: 'auto',
+  },
+})
+
+const Name = styled.p(({theme}) => ({
+  color: theme.color.gray600,
+  fontWeight: 700,
+  fontSize: '2rem',
+}))
+
+interface LabelProps {
+  color: keyof Theme['color']
+}
+
+const Label = styled.p<LabelProps>(({theme, color}) => ({
+  color: theme.color[color],
+  fontWeight: 500,
+  fontSize: '1.6rem',
+}))
+
+const MultipleDescriptionContainer = styled.div(({theme}) => ({
+  display: 'flex',
+  alignItems: 'center',
+  '& > ::before': {
+    content: '""',
+    height: '1rem',
+    border: `1px solid ${theme.color.gray300}`,
+    borderRadius: '5rem',
+    margin: '0 1rem',
+  },
+  '& > :first-of-type::before': {
+    border: 'none',
+    margin: 0,
+  },
+}))
+
+const IconLabelContainer = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+  '& > * + *': {marginLeft: '0.5rem'},
+})
+
+interface IconLabelProps extends LabelProps {
+  icon: IconName
+}
+
+function IconLabel({icon, color, children}: PropsWithChildren<IconLabelProps>) {
+  return (
+    <IconLabelContainer>
+      <Icon name={icon} color={color} width="2rem" height="2rem" />
+      <Label color={color}>{children}</Label>
+    </IconLabelContainer>
+  )
+}
+
+const ProfilePic = styled.img(({theme}) => ({
+  flex: '0 0 10rem',
+  width: '10rem',
+  height: '10rem',
+  borderRadius: '50%',
+  overflow: 'hidden',
+  backgroundColor: theme.color.gray300,
+}))
+
+interface CardProps extends HTMLAttributes<HTMLDivElement>, BaseContainerProps {}
+
+interface ReservationResultProps extends CardProps {
+  instructorName: string
+  academyName: string
+  pricePerHour: number
+  image: string
+  distance: number
+  duration: number
+  rating: number
+  onRequestReservation?: () => void
+  onContact?: () => void
+}
+
+export function ReservationResultCard({
+  instructorName,
+  academyName,
+  pricePerHour,
+  image,
+  distance,
+  duration,
+  rating,
+  onRequestReservation,
+  onContact,
+  ...props
+}: ReservationResultProps) {
+  return (
+    <BaseContainer {...props}>
+      <ProfilePic src={image} />
+      <ContentContainer>
+        <Name>{instructorName}</Name>
+        <IconLabel icon="building" color="gray500">
+          {academyName}
+        </IconLabel>
+        <MultipleDescriptionContainer>
+          <IconLabel icon="payments" color="gray500">
+            {`${pricePerHour}원`}
+          </IconLabel>
+          <IconLabel icon="duration" color="gray500">
+            {`${duration}시간`}
+          </IconLabel>
+          <IconLabel icon="building" color="gray500">
+            {`${distance}km`}
+          </IconLabel>
+          <IconLabel icon="star" color="gray500">
+            {rating}
+          </IconLabel>
+        </MultipleDescriptionContainer>
+        <ActionsContainer>
+          <Button onClick={onContact}>문의</Button>
+          <Button onClick={onRequestReservation}>예약</Button>
+        </ActionsContainer>
+      </ContentContainer>
+    </BaseContainer>
+  )
+}
+
+interface StudentHistoryProps extends CardProps {
+  instructorName: string
+  academyName: string
+  image: string
+  dateStr: string
+  timeStr: string
+  onReviewClick?: () => void
+}
+
+export function StudentHistoryCard({
+  instructorName,
+  academyName,
+  dateStr,
+  timeStr,
+  image,
+  onReviewClick,
+  ...props
+}: StudentHistoryProps) {
+  return (
+    <BaseContainer {...props}>
+      <ProfilePic src={image} />
+      <ContentContainer>
+        <Name>{instructorName}</Name>
+        <IconLabel icon="building" color="gray500">
+          {academyName}
+        </IconLabel>
+        <MultipleDescriptionContainer>
+          <Label color="gray600">{dateStr}</Label>
+          <Label color="gray600">{timeStr}</Label>
+        </MultipleDescriptionContainer>
+        {onReviewClick && (
+          <ActionsContainer>
+            <Button onClick={onReviewClick}>리뷰 작성하기</Button>
+          </ActionsContainer>
+        )}
+      </ContentContainer>
+    </BaseContainer>
+  )
+}
+
+interface InstructorHistoryProps extends CardProps {
+  studentName: string
+  phoneStr: string
+  dateStr: string
+  timeStr: string
+  image: string
+  onRejectClick?: () => void
+}
+
+export function InstructorHistoryCard({
+  studentName,
+  phoneStr,
+  dateStr,
+  timeStr,
+  image,
+  onRejectClick,
+  ...props
+}: InstructorHistoryProps) {
+  return (
+    <BaseContainer {...props}>
+      <ProfilePic src={image} />
+      <ContentContainer>
+        <Name>{studentName}</Name>
+        <IconLabel icon="call" color="gray500">
+          {phoneStr}
+        </IconLabel>
+        <MultipleDescriptionContainer>
+          <Label color="gray600">{dateStr}</Label>
+          <Label color="gray600">{timeStr}</Label>
+        </MultipleDescriptionContainer>
+        {onRejectClick && (
+          <ActionsContainer>
+            <Button onClick={onRejectClick} bgColor="warning">
+              거절
+            </Button>
+          </ActionsContainer>
+        )}
+      </ContentContainer>
+    </BaseContainer>
+  )
+}
