30e9e00d442c7daf12ed4557a51051e5b753bff3
jsLeen <lejs1008@naver.com>
2024-02-13 19:19:07 +0900

    [BE] feat : dto 분리에 따른 서비스 계층 코드 변경

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationListService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationListService.java
@@ -1,13 +1,13 @@
 package com.drivingtoday.domain.reservation;
 
-import com.drivingtoday.domain.reservation.dto.ReservationDetailResponse;
+import com.drivingtoday.domain.reservation.dto.ReservationInstructorResponse;
+import com.drivingtoday.domain.reservation.dto.ReservationStudentResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,31 +17,21 @@ public class ReservationListService {
     private final ReservationRepository reservationRepository;
 
     @Transactional
-    public List<ReservationDetailResponse> findAllStudentReservation(Long studentId, Integer pageNumber, Integer pageSize){
+    public List<ReservationStudentResponse> findAllStudentReservation(Long studentId, Integer pageNumber, Integer pageSize){
 
         List<Reservation> reservationList = reservationRepository.findAllByStudentId(studentId,
                 PageRequest.of(pageNumber - 1, pageSize, Sort.by("createdAt").descending())).getContent();
-        List<ReservationDetailResponse> reservationDetailResponses = new ArrayList<>();
 
-        reservationList.forEach(reservation -> {
-            reservationDetailResponses.add(ReservationDetailResponse.from(reservation));
-        });
-
-        return reservationDetailResponses;
+        return reservationList.stream().map(ReservationStudentResponse::from).toList();
     }
 
     @Transactional
-    public List<ReservationDetailResponse> findAllInstructorReservation(Long instructorId, Integer pageNumber, Integer pageSize){
+    public List<ReservationInstructorResponse> findAllInstructorReservation(Long instructorId, Integer pageNumber, Integer pageSize){
 
         List<Reservation> reservationList = reservationRepository.findAllByInstructorId(instructorId,
                 PageRequest.of(pageNumber - 1, pageSize, Sort.by("createdAt").descending())).getContent();
-        List<ReservationDetailResponse> reservationDetailResponses = new ArrayList<>();
 
-        reservationList.forEach(reservation -> {
-            reservationDetailResponses.add(ReservationDetailResponse.from(reservation));
-        });
-
-        return reservationDetailResponses;
+        return reservationList.stream().map(ReservationInstructorResponse::from).toList();
     }
 
 
