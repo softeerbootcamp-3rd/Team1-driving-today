bb5ae31c3febd25e7018491aa268a30c8de73650
zensen6 <junhee1469@gmail.com>
2024-02-13 17:14:10 +0900

    [BE] feat : student dto 형식 통일함

--- backend/driving-today/src/main/java/com/drivingtoday/domain/student/dto/StudentInfo.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/student/dto/StudentInfo.java
@@ -1,2 +1,23 @@
-package com.drivingtoday.domain.student.dto;public class StudentInfo {
+package com.drivingtoday.domain.student.dto;
+
+import com.drivingtoday.domain.student.Student;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class StudentInfo {
+    private String name;
+    private String phoneNumber;
+    private String studentImage;
+    private String nickname;
+
+    public static StudentInfo from(Student student) {
+        return StudentInfo.builder()
+                .name(student.getName())
+                .phoneNumber(student.getPhoneNumber())
+                .studentImage(student.getStudentImage())
+                .nickname(student.getNickname())
+                .build();
+    }
 }
