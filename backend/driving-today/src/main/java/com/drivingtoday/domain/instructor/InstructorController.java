e6110dddeb54d27000d6c52878c4826093ede39c
jsLeen <lejs1008@naver.com>
2024-02-13 12:22:10 +0900

    [BE] feat : 예약 가능한 강사 목록 보여주기 (#48)

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorController.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorController.java
@@ -1,11 +1,18 @@
 package com.drivingtoday.domain.instructor;
 
 import com.drivingtoday.domain.instructor.dto.InstructorDetailResponse;
+import com.drivingtoday.domain.instructor.dto.AvailableInstructorsRequest;
+import com.drivingtoday.domain.instructor.dto.AvailableInstructorInfo;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class InstructorController {
@@ -17,5 +24,13 @@ public class InstructorController {
         InstructorDetailResponse response = instructorFindService.findInstructor(instructorId);
         return ResponseEntity.ok(response);
     }
-}
 
+    @GetMapping("/instructors")
+    @Operation(summary = "예약 가능한 강사 리스트 보기 API")
+    public ResponseEntity<List<AvailableInstructorInfo>> instructorList(@ModelAttribute @Valid AvailableInstructorsRequest request) {
+
+        List<AvailableInstructorInfo> availableInstructors =
+                instructorFindService.findAvailableInstructors(request);
+        return ResponseEntity.ok(availableInstructors);
+    }
+}
--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorFindService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorFindService.java
@@ -4,18 +4,37 @@ import com.drivingtoday.domain.instructor.dto.InstructorDetailResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.drivingtoday.domain.instructor.dto.AvailableInstructorInfo;
+import com.drivingtoday.domain.instructor.dto.AvailableInstructorsRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class InstructorFindService {
 
     private final InstructorRepository instructorRepository;
-
     @Transactional
     public InstructorDetailResponse findInstructor(Long instructorId) {
         Instructor instructor = instructorRepository.findById(instructorId)
                 .orElseThrow(() -> new RuntimeException("해당 강사가 존재하지 않습니다."));
         return InstructorDetailResponse.from(instructor);
     }
-}
 
+    @Transactional
+    public List<AvailableInstructorInfo> findAvailableInstructors(AvailableInstructorsRequest request) {
+        return instructorRepository.findAvailableInstructors(
+                request.getReservationDate().toString(),
+                request.getReservationTime(),
+                request.getTrainingTime(),
+                request.getLatitude(),
+                request.getLongitude(),
+                PageRequest.of(request.getPageNumber() - 1, request.getPageSize())
+                // 0 페이지부터 시작이 default여서 -1 처리
+        );
+    }
+}
