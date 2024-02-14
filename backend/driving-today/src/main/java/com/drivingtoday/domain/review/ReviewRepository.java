331f93ffa1a260931a26850a1b14c2c54f59a56b
jsLeen <lejs1008@naver.com>
2024-02-14 18:27:02 +0900

    [BE] feat : 강사 id로 강사의 리뷰 평점 조회하는 쿼리 추가

--- backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewRepository.java
@@ -3,7 +3,12 @@ package com.drivingtoday.domain.review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findAllByInstructorId(Long instructorId, Pageable pageable);
+
+    // 특정 강사의 리뷰 평균 평점을 가져오는 쿼리
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.instructor.id = :instructorId")
+    Double findAverageRatingByInstructorId(Long instructorId);
 }
