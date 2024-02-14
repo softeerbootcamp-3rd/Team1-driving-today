16a92b974f96dc7f52e09df3f379a65c198692e8
seojeewon <readyjiwon@gmail.com>
2024-02-14 11:36:36 +0900

    [BE] feat : Jwt 인증 수행하는 JwtProvider 생성
    인증 수행 외에도, 토큰 생성, Jwt 인스턴스 생성, jwt 속성 getter 등의 기능도 있음

---
+++ backend/driving-today/src/main/java/com/drivingtoday/global/auth/jwt/JwtProvider.java
@@ -0,0 +1,90 @@
+package com.drivingtoday.global.auth.jwt;
+
+import com.drivingtoday.global.auth.constants.JwtErrorCode;
+import io.jsonwebtoken.*;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class JwtProvider {
+    public static final String CLAIM_USERID = "userId";
+    public static final String CLAIM_ROLE = "role";
+    public static final String BEARER_TOKEN_PREFIX = "Bearer ";
+
+    @Getter
+    private final String key;
+
+    @Value("${jwt.access-token.expire-length}")
+    private Long accessTokenExpireLengthInSeconds;
+
+    @Value("${jwt.refresh-token.expire-length}")
+    private Long refreshTokenExpireLengthInSeconds;
+
+    public JwtProvider(@Value("${jwt.credentials.secret-key}") String secretKey) {
+        this.key = secretKey;
+    }
+
+    public String createToken(Map<String, Object> claims, Date expireDate) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+
+    public Jwt createJwt(Map<String, Object> claims) {
+        String accessToken = createToken(claims, getExpireDateAccessToken());
+        String refreshToken = createToken(new HashMap<>(), getExpireDateRefreshToken());
+        return Jwt.of(accessToken, refreshToken);
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Date getExpireDateAccessToken() {
+        long expireTimeMils = 1000L * accessTokenExpireLengthInSeconds;
+        return new Date(System.currentTimeMillis() + expireTimeMils);
+    }
+
+    public Date getExpireDateRefreshToken() {
+        long expireTimeMils = 1000L * refreshTokenExpireLengthInSeconds;
+        return new Date(System.currentTimeMillis() + expireTimeMils);
+    }
+    
+    public String resolveToken(String authorization) {
+        return authorization.split(" ")[1];
+    }
+
+    public JwtErrorCode validateToken(String token) {
+        try {
+            getClaims(token);
+            return JwtErrorCode.VALID_JWT_TOKEN;
+        } catch (SignatureException exception) {
+            log.info("JWT 서명이 유효하지 않습니다");
+            return JwtErrorCode.INVALID_JWT_SIGNATURE;
+        } catch (MalformedJwtException exception) {
+            log.info("JWT가 올바르게 구성되지 않았습니다.");
+            return JwtErrorCode.INVALID_JWT_TOKEN;
+        } catch (ExpiredJwtException exception) {
+            log.info("만료된 토큰입니다.");
+            return JwtErrorCode.EXPIRED_JWT_TOKEN;
+        } catch (UnsupportedJwtException exception) {
+            log.info("지원되지 않는 형식의 토큰입니다.");
+            return JwtErrorCode.UNSUPPORTED_JWT_TOKEN;
+        } catch (IllegalArgumentException exception) {
+            log.info("JWT Claim이 비어있습니다.");
+            return JwtErrorCode.EMPTY_JWT;
+        }
+    }
+}
