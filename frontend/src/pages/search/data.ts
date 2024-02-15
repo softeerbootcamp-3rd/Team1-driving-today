b71367003cc5332df65c47747112f37ce1602713
Jeongjin Oh <rojay.developer@gmail.com>
2024-02-15 22:50:10 +0900

    chore: mock data

--- frontend/src/pages/search/data.ts
+++ frontend/src/pages/search/data.ts
@@ -110,3 +110,99 @@ export const dummydata = [
     academyName: '강남차동자전운문전학원',
   },
 ]
+
+export const detailData = {
+  instructorInfo: {
+    id: 10,
+    name: '오정진',
+    phoneNumber: '010-1234-5678',
+    instructorImage: 'https://picsum.photos/200',
+    pricePerHour: 12000,
+    introduction:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a fringilla est, at vulputate quam. Proin eget quam sed nulla vehicula vulputate.',
+  },
+  academyInfo: {
+    name: '강남차동자전운문전학원',
+    latitude: 37.492,
+    longitude: 127.012,
+    cert: true,
+  },
+}
+
+export const reviewData = [
+  {
+    reviewId: 57,
+    contents:
+      '서비스가 매우 만족스럽습니다. 친절한 강사와 효과적인 수업이었습니다. 다만 시설이 조금 낡아서 아쉬웠어요.',
+    rating: 4,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '이지수',
+  },
+  {
+    reviewId: 26,
+    contents:
+      '교육 내용은 훌륭하지만 강사의 태도가 별로였습니다. 고칠 점이 많아 보입니다. 시설은 깨끗하고 좋았습니다.',
+    rating: 2.5,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '김준희',
+  },
+  {
+    reviewId: 83,
+    contents:
+      '매우 만족스러운 수업이었습니다. 강사님의 친절한 설명과 배려가 돋보였습니다. 시설도 깨끗하고 좋았습니다.',
+    rating: 5,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '서지원',
+  },
+  {
+    reviewId: 39,
+    contents: '수업은 만족스러웠지만 시설이 조금 협소해서 불편했습니다. 강사님은 친절하셨습니다.',
+    rating: 3.5,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '홍정희',
+  },
+  {
+    reviewId: 74,
+    contents: '수업은 효과적이었으나 강사의 태도가 별로였습니다. 더 좋은 서비스를 원합니다.',
+    rating: 2,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '김준희',
+  },
+  {
+    reviewId: 10,
+    contents:
+      '전반적으로 좋았습니다. 하지만 수업 시간이 너무 짧았어요. 좀 더 긴 시간으로 수업을 받고 싶었습니다.',
+    rating: 4.5,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '이지수',
+  },
+  {
+    reviewId: 92,
+    contents: '수업은 매우 만족스럽습니다. 친절한 강사님과 좋은 시설을 제공해 주셔서 감사합니다.',
+    rating: 5,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '홍정희',
+  },
+  {
+    reviewId: 69,
+    contents:
+      '서비스가 좋았습니다. 하지만 수업 시간이 너무 짧아서 아쉬웠습니다. 더 긴 시간으로 수업을 받고 싶었습니다.',
+    rating: 4,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '서지원',
+  },
+  {
+    reviewId: 5,
+    contents: '전반적으로 만족스러운 수업이었습니다. 강사님의 친절함에 감사드립니다.',
+    rating: 4,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '김준희',
+  },
+  {
+    reviewId: 31,
+    contents: '강사님의 설명이 너무 어려웠습니다. 좀 더 쉽게 설명해 주셨으면 좋겠습니다.',
+    rating: 2.5,
+    createdAt: '2024-02-14T17:13:58.586Z',
+    reviewerName: '이지수',
+  },
+]
