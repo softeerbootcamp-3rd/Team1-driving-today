d18dc4efb87b6950134d2de94d8e0a65d51ac542
zensen6 <junhee1469@gmail.com>
2024-02-15 12:05:56 +0900

    [BE] feat : reservation handling error code 작성

---
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/exception/ReservationErrorCode.java
@@ -0,0 +1,19 @@
+package com.drivingtoday.domain.reservation.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ReservationErrorCode {
+
+    STUDENT_NOT_EXISTS("E2011",HttpStatus.UNPROCESSABLE_ENTITY, "등록자가 존재하지 않습니다."),
+    INSTRUCTOR_NOT_EXISTS("E2013", HttpStatus.UNPROCESSABLE_ENTITY, "예약하려는 강사가 존재하지 않습니다."),
+    RESERVATION_ALREADY_EXISTS("E2014", HttpStatus.CONFLICT, "이미 같은시간대에 예약이 존재하여 접수할 수 없습니다.");
+
+    private String errorCode;
+    private HttpStatus httpStatus;
+    private String message;
+
+}
