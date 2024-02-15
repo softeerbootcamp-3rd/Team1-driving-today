edb71d15949da020fcb158b663bd572c43aeac53
jsLeen <lejs1008@naver.com>
2024-02-15 15:34:04 +0900

    [BE] feat : 예약 삭제 서비스 예외 처리 변경

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationDeleteService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationDeleteService.java
@@ -1,6 +1,8 @@
 package com.drivingtoday.domain.reservation;
 
 import com.drivingtoday.domain.instructor.Instructor;
+import com.drivingtoday.domain.reservation.exception.ReservationErrorCode;
+import com.drivingtoday.domain.reservation.exception.ReservationException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,11 +20,9 @@ public class ReservationDeleteService {
     @Transactional
     public void deleteReservation(Long reservationId){
 
-        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new RuntimeException("reservation doesn't exists"));
-
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(() -> ReservationException.from(ReservationErrorCode.RESERVATION_NOT_EXISTS));
         reservationRepository.delete(reservation);
-
-        return;
     }
 
 }
