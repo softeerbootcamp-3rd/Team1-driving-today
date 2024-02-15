07d53137113b47b698ea72b7f13671239372fe5a
junhea <97426534+junhea@users.noreply.github.com>
2024-02-15 14:56:09 +0900

    feat: dashboard page

--- frontend/src/pages/dashboard/page.tsx
+++ frontend/src/pages/dashboard/page.tsx
@@ -1,7 +1,67 @@
+import styled from '@emotion/styled'
+
+import {Header} from '@/components/header'
+import {Logo} from '@/components/logo'
+
+import {Cardlist} from './components/cardlist/cardlist'
+import {MyProfileCard} from './components/myprofile-card'
+import {ScheduleCard} from './components/schedule-card'
+
 export function StudentDashboard() {
-  return <>student dashboard</>
+  return (
+    <RootLayout>
+      <Header>
+        <Logo />
+      </Header>
+      <ContentLayout>
+        <ContentContainer>
+          <ScheduleCard />
+        </ContentContainer>
+        <ModalContainer>
+          <MyProfileCard />
+        </ModalContainer>
+      </ContentLayout>
+    </RootLayout>
+  )
 }
 
 export function InstructorDashboard() {
-  return <>instructor dashboard</>
+  return (
+    <RootLayout>
+      <Header>
+        <Logo />
+      </Header>
+      <ContentLayout>
+        <ContentContainer>
+          <Cardlist />
+        </ContentContainer>
+        <ModalContainer>
+          <MyProfileCard />
+        </ModalContainer>
+      </ContentLayout>
+    </RootLayout>
+  )
 }
+
+const RootLayout = styled.div({display: 'flex', flexDirection: 'column', flexGrow: 1})
+const ContentLayout = styled.div({
+  display: 'flex',
+  gap: '1rem',
+  flexGrow: 1,
+  justifyContent: 'center',
+  margin: '0 20rem',
+  overflowY: 'hidden',
+})
+
+const ContentContainer = styled.div({
+  display: 'flex',
+  flexGrow: 1,
+  minWidth: '50rem',
+  padding: '1rem',
+})
+
+const ModalContainer = styled.div({
+  minWidth: '40rem',
+  padding: '1rem',
+  overflowY: 'auto',
+})
