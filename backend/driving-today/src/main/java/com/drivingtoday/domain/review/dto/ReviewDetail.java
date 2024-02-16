c0f15599c178af56550a1c61e18ca645665a4eb0
jsLeen <lejs1008@naver.com>
2024-02-16 10:20:06 +0900

    [BE] feat : Review 상세 정보 응답에 리뷰 작성자 사진 정보 추가

--- backend/driving-today/src/main/java/com/drivingtoday/domain/review/dto/ReviewDetail.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/review/dto/ReviewDetail.java
@@ -9,17 +9,19 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class ReviewDetail {
-    private String reviewerName;
     private String contents;
     private Double rating;
     private LocalDateTime createdAt;
+    private String reviewerName;
+    private String reviewerImage;
 
     public static ReviewDetail from(Review review) {
         return ReviewDetail.builder()
-                .reviewerName(review.getStudent().getName())
                 .contents(review.getContents())
                 .rating(review.getRating())
                 .createdAt(review.getCreatedAt())
+                .reviewerName(review.getStudent().getName())
+                .reviewerImage(review.getStudent().getStudentImage())
                 .build();
     }
 }
