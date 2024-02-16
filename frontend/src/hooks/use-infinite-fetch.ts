a4ab7c290cdcf4ccb519c41f0e55948dcc80c593
Jeongjin Oh <rojay.developer@gmail.com>
2024-02-16 11:40:52 +0900

    wip: use infinite fetch

--- frontend/src/hooks/use-infinite-fetch.ts
+++ frontend/src/hooks/use-infinite-fetch.ts
@@ -1,22 +1,37 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
-interface UseInfiniteFetchArg {
-  initialParams: unknown
-  getNextPageParams: () => void
+interface UseInfiniteFetchArg<TData, TError, TPageParams> {
+  queryFn: ({pageParam}: {pageParam: TPageParams}) => Promise<TData>
+  initialParams: TPageParams
+  getNextPageParams: ({pageParam}: {pageParam: TPageParams}) => TPageParams | undefined | null
 }
 
 interface UseInfniteFetchReturn<TData> {
-  fetchNextPage: () => void
+  fetchNextPage: () => Promise<unknown>
   data?: TData
   error?: unknown
   loading: boolean
 }
 
-export function useInfiniteFetch<TData>(arg: UseInfiniteFetchArg): UseInfniteFetchReturn<TData> {
-  const [data, setData] = useState()
+export function useInfiniteFetch<TData, TError, TPageParams>({
+  queryFn,
+  initialParams,
+  getNextpageParams,
+}: UseInfiniteFetchArg<TData, TError, TPageParams>): UseInfniteFetchReturn<TData> {
+  const [data, setData] = useState<TData>()
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState()
+  const nextParams = initialParams
 
-  const fetchNextPage = () => {}
+  const fetchNextPage = async () => {
+    try {
+      const data = await queryFn({pageParam: nextParams})
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    const abortController = new AbortController()
+  }, [])
+
   return {fetchNextPage, data, loading, error}
 }
