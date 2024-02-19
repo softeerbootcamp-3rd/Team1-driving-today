9bed556762939847944e9c9ac5a990cf4f8da3da
서지원 <readyjiwon@gmail.com>
2024-02-19 20:03:58 +0900

    feat: 연수생용, 강사용 api에 각각 /student, /instructor 추가 (#179)

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorController.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorController.java
@@ -18,14 +18,14 @@ import java.util.List;
 public class InstructorController {
     private final InstructorFindService instructorFindService;
 
-    @GetMapping("/instructors/{instructor_id}")
+    @GetMapping("/student/instructors/{instructor_id}")
     @Operation(summary = "강사 상세 정보 반환하는 API")
     public ResponseEntity<InstructorDetailResponse> instructorDetails(@PathVariable("instructor_id") Long instructorId) {
         InstructorDetailResponse response = instructorFindService.findInstructor(instructorId);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/instructors")
+    @GetMapping("/student/instructors")
     @Operation(summary = "예약 가능한 강사 리스트 보기 API")
     public ResponseEntity<List<AvailableInstructorInfo>> instructorList(@ModelAttribute @Valid AvailableInstructorsRequest request) {
 
