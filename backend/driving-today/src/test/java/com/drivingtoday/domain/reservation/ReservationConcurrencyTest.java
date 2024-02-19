23fff21f3b3196223257625f1c0a6a3e8636eaef
jsLeen <lejs1008@naver.com>
2024-02-19 23:53:56 +0900

    [BE] test : 동시성 처리 이후 테스트 코드 추가

--- backend/driving-today/src/test/java/com/drivingtoday/domain/reservation/ReservationConcurrencyTest.java
+++ backend/driving-today/src/test/java/com/drivingtoday/domain/reservation/ReservationConcurrencyTest.java
@@ -22,6 +22,7 @@ public class ReservationConcurrencyTest {
 
     @Autowired ReservationAddService reservationAddService;
     @Autowired ReservationRepository reservationRepository;
+    @Autowired ReservationLockFacade reservationLockFacade;
 
     @Test
     public void 동시에_100명이_겹치는_시간대_예약_요청_동시성_처리X() throws InterruptedException {
@@ -49,5 +50,32 @@ public class ReservationConcurrencyTest {
         List<Reservation> reservations = reservationRepository.findAllByInstructorId(2L);
         assertThat(reservations.size()).isNotEqualTo(1);
         assertThat(reservations.size()).isEqualTo(100- count.get());
+    }
+
+    @Test
+    public void 동시성_처리_이후() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        CountDownLatch countDownLatch = new CountDownLatch(100);
+        AtomicInteger count = new AtomicInteger(0);
+
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(() -> {
+                try {
+                    ReservationRequest reservationRequest =
+                            new ReservationRequest(LocalDate.of(2024, 3, 17),
+                                    9,  2, 2L);
+
+                    reservationLockFacade.addReservation(reservationRequest, 2L);
+                } catch (ReservationException e) {
+                    count.incrementAndGet();
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+
+        List<Reservation> reservations = reservationRepository.findAllByInstructorId(2L);
+        assertThat(reservations.size()).isEqualTo(1);
     }
 }
