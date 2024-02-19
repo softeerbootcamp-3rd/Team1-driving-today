3a1e88a2e7ae5ca201ba93242404d04b6976b5b1
zensen6 <junhee1469@gmail.com>
2024-02-19 23:43:04 +0900

    [BE] feat : chatMessage repository 추가

--- backend/driving-today/src/main/java/com/drivingtoday/domain/chat/ChatMessageRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/chat/ChatMessageRepository.java
@@ -1,2 +1,11 @@
-package com.drivingtoday.domain.chat;public interface ChatMessageRepository {
+package com.drivingtoday.domain.chat;
+
+import com.drivingtoday.domain.chat.model.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+    List<ChatMessage> findAllByRoomId(String roomId);
+
 }
