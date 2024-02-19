a370df62f456543cdfda3e622dc6a979e63d4513
zensen6 <junhee1469@gmail.com>
2024-02-19 23:45:50 +0900

    [BE] feat : ChatRoomDTO 작성

--- backend/driving-today/src/main/java/com/drivingtoday/domain/chat/model/ChatRoomInfoDetail.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/chat/model/ChatRoomInfoDetail.java
@@ -1,2 +1,27 @@
-package com.drivingtoday.domain.chat.model;public class ChatRoomInfoDetail {
+package com.drivingtoday.domain.chat.model;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+public class ChatRoomInfoDetail {
+
+    @NotNull
+    private ChatRoomInfo chatRoomInfo;
+
+    @Builder.Default
+    private List<ChatMessage> chatMessageList = new ArrayList<>();
+
+    public static ChatRoomInfoDetail from(ChatRoomInfo chatRoomInfo, List<ChatMessage> chatMessageList){
+        return ChatRoomInfoDetail.builder()
+                .chatRoomInfo(chatRoomInfo)
+                .chatMessageList(chatMessageList)
+                .build();
+    }
 }
