8052f70738fb83758bef68c1ec7e89dd16d91fa6
jsLeen <lejs1008@naver.com>
2024-02-20 23:52:21 +0900

    [BE] 리뷰 관련 에러 처리

--- backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewAddService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewAddService.java
@@ -2,6 +2,8 @@ package com.drivingtoday.domain.review;
 
 import com.drivingtoday.domain.reservation.Reservation;
 import com.drivingtoday.domain.reservation.ReservationRepository;
+import com.drivingtoday.domain.reservation.exception.ReservationErrorCode;
+import com.drivingtoday.domain.reservation.exception.ReservationException;
 import com.drivingtoday.domain.review.dto.ReviewRequest;
 import com.drivingtoday.domain.review.exception.ReviewErrorCode;
 import com.drivingtoday.domain.review.exception.ReviewException;
@@ -18,11 +20,13 @@ public class ReviewAddService {
     @Transactional
     public Long addReview(ReviewRequest reviewRequest, Long studentId) {
         Reservation reservation = reservationRepository.findById(reviewRequest.getReservationId())
-                .orElseThrow(() -> ReviewException.from(ReviewErrorCode.NOT_EXIST_RESERVATION_REVIEW));
+                .orElseThrow(() -> ReservationException.from(ReservationErrorCode.RESERVATION_NOT_EXISTS));
+
         //예약자와 요청을 보낸 학생이 일치하지 않은 경우 예외 발생
         if (!reservation.getStudent().getId().equals(studentId)) {
             throw ReviewException.from(ReviewErrorCode.INVALID_REVIEWER);
         }
+
         Review review = reviewRequest.toReview(reservation.getStudent(), reservation.getInstructor());
         reviewRepository.save(review);
         return review.getId();
