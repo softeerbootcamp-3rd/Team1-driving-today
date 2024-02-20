cb53669383742ca25d65a10e5d7dc0177c609d44
seojeewon <readyjiwon@gmail.com>
2024-02-20 17:15:51 +0900

    [BE] refactor : findAvailableInstructors 매개변수 리팩토링

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorFindService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorFindService.java
@@ -1,17 +1,14 @@
 package com.drivingtoday.domain.instructor;
 
+import com.drivingtoday.domain.instructor.dto.AvailableInstructorInfo;
 import com.drivingtoday.domain.instructor.dto.InstructorDetailResponse;
 import com.drivingtoday.domain.review.ReviewRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import com.drivingtoday.domain.instructor.dto.AvailableInstructorInfo;
-import com.drivingtoday.domain.instructor.dto.AvailableInstructorsRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,6 +18,7 @@ public class InstructorFindService {
 
     private final InstructorRepository instructorRepository;
     private final ReviewRepository reviewRepository;
+
     @Transactional
     public InstructorDetailResponse findInstructor(Long instructorId) {
         Instructor instructor = instructorRepository.findById(instructorId)
@@ -30,20 +28,14 @@ public class InstructorFindService {
     }
 
     @Transactional
-    public List<AvailableInstructorInfo> findAvailableInstructors(AvailableInstructorsRequest request) {
-        return instructorRepository.findAvailableInstructors(
-                request.getReservationDate().toString(),
-                request.getReservationTime(),
-                request.getTrainingTime(),
-                request.getLatitude(),
-                request.getLongitude(),
-                PageRequest.of(request.getPageNumber() - 1, request.getPageSize())
-                // 0 페이지부터 시작이 default여서 -1 처리
-        );
+    public List<AvailableInstructorInfo> findAvailableInstructors(Double latitude, Double longitude, LocalDate reservationDate,
+                                                                  Integer reservationTime, Integer trainingTime, Integer pageNumber, Integer pageSize) {
+        return instructorRepository.findAvailableInstructors(reservationDate.toString(), reservationTime, trainingTime,
+                latitude, longitude, PageRequest.of(pageNumber - 1, pageSize));
     }
 
     @Transactional
-    public Instructor findById(Long instructorId){
+    public Instructor findById(Long instructorId) {
         Optional<Instructor> instructor = instructorRepository.findById(instructorId);
         if (instructor.isPresent()) {
             return instructor.get();
