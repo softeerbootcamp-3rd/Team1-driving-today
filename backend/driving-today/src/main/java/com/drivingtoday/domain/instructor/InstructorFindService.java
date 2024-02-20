02a157bb27a425cb3a61b72dfe810460403cb83d
서지원 <readyjiwon@gmail.com>
2024-02-20 17:55:44 +0900

    feat : 로그인한 연수생, 강사 정보 반환 API (#201)

--- backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorFindService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/instructor/InstructorFindService.java
@@ -2,6 +2,7 @@ package com.drivingtoday.domain.instructor;
 
 import com.drivingtoday.domain.instructor.dto.AvailableInstructorInfo;
 import com.drivingtoday.domain.instructor.dto.InstructorDetailResponse;
+import com.drivingtoday.domain.instructor.dto.InstructorInfo;
 import com.drivingtoday.domain.review.ReviewRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +21,18 @@ public class InstructorFindService {
     private final ReviewRepository reviewRepository;
 
     @Transactional
-    public InstructorDetailResponse findInstructor(Long instructorId) {
+    public InstructorDetailResponse findInstructorDetails(Long instructorId) {
         Instructor instructor = instructorRepository.findById(instructorId)
                 .orElseThrow(() -> new RuntimeException("해당 강사가 존재하지 않습니다."));
         Double averageRating = reviewRepository.findAverageRatingByInstructorId(instructorId);
         return InstructorDetailResponse.of(instructor, averageRating);
+    }
+
+    @Transactional
+    public InstructorInfo findInstructor(Long instructorId) {
+        Instructor instructor = instructorRepository.findById(instructorId)
+                .orElseThrow(() -> new RuntimeException("해당 강사가 존재하지 않습니다."));
+        return InstructorInfo.from(instructor);
     }
 
     @Transactional
