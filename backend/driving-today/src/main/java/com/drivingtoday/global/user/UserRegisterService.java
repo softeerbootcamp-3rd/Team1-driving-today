88991925c2a858397d1ddef60597e78dd232d6b4
seojeewon <readyjiwon@gmail.com>
2024-02-20 17:09:04 +0900

    [BE] refactor : 회원가입 관련 코드 네이밍에서 join->register로 변경

--- backend/driving-today/src/main/java/com/drivingtoday/global/user/UserRegisterService.java
+++ backend/driving-today/src/main/java/com/drivingtoday/global/user/UserRegisterService.java
@@ -7,8 +7,8 @@ import com.drivingtoday.domain.instructor.InstructorRepository;
 import com.drivingtoday.domain.student.Student;
 import com.drivingtoday.domain.student.StudentRepository;
 import com.drivingtoday.global.s3.S3UploadService;
-import com.drivingtoday.global.user.dto.InstructorJoinRequest;
-import com.drivingtoday.global.user.dto.StudentJoinRequest;
+import com.drivingtoday.global.user.dto.InstructorRegisterRequest;
+import com.drivingtoday.global.user.dto.StudentRegisterRequest;
 import com.drivingtoday.global.user.exception.UserErrorCode;
 import com.drivingtoday.global.user.exception.UserException;
 import lombok.RequiredArgsConstructor;
@@ -19,21 +19,22 @@ import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
-public class UserJoinService {
+public class UserRegisterService {
+    
     private final S3UploadService s3UploadService;
     private final StudentRepository studentRepository;
     private final InstructorRepository instructorRepository;
     private final AcademyRepository academyRepository;
 
-    public Long addStudent(StudentJoinRequest joinRequest, MultipartFile profileImg) {
+    public Long addStudent(StudentRegisterRequest registerRequest, MultipartFile profileImg) {
 
         //이미 존재하는 이메일일 경우 예외 발생
-        if(studentRepository.findByEmail(joinRequest.getEmail()).isPresent()){
+        if(studentRepository.findByEmail(registerRequest.getEmail()).isPresent()){
             throw UserException.from(UserErrorCode.DUPLICATED_USER_EMAIL);
         }
 
         //이미 존재하는 닉네임일 경우 예외 발생
-        if (studentRepository.findByNickname(joinRequest.getNickname()).isPresent()) {
+        if (studentRepository.findByNickname(registerRequest.getNickname()).isPresent()) {
             throw UserException.from(UserErrorCode.DUPLICATED_USER_NICKNAME);
         }
 
@@ -48,15 +49,15 @@ public class UserJoinService {
         }
 
         //데이터베이스에 학생 저장
-        Student newStudent = joinRequest.toStudent(imgUrl);
+        Student newStudent = registerRequest.toStudent(imgUrl);
         studentRepository.save(newStudent);
         return newStudent.getId();
     }
 
-    public Long addInstructor(InstructorJoinRequest joinRequest, MultipartFile profileImg) {
+    public Long addInstructor(InstructorRegisterRequest registerRequest, MultipartFile profileImg) {
 
         //이미 존재하는 이메일일 경우 예외 발생
-        if(instructorRepository.findByEmail(joinRequest.getEmail()).isPresent()){
+        if(instructorRepository.findByEmail(registerRequest.getEmail()).isPresent()){
             throw UserException.from(UserErrorCode.DUPLICATED_USER_EMAIL);
         }
         //프로필 이미지 업로드
@@ -69,8 +70,8 @@ public class UserJoinService {
             throw new RuntimeException("S3 upload Fail");
         }
         //데이터베이스에 강사 저장
-        Academy academy = academyRepository.findById(joinRequest.getAcademyId()).orElseThrow(() -> new RuntimeException("존재하지 않는 학원입니다."));
-        Instructor instructor = joinRequest.toInstructor(academy, imgUrl);
+        Academy academy = academyRepository.findById(registerRequest.getAcademyId()).orElseThrow(() -> new RuntimeException("존재하지 않는 학원입니다."));
+        Instructor instructor = registerRequest.toInstructor(academy, imgUrl);
         instructorRepository.save(instructor);
         return instructor.getId();
     }
