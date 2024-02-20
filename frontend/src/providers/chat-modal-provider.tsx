96c557036b0b87b6657bc2b06d873fc4bdd5db8a
junhea <97426534+junhea@users.noreply.github.com>
2024-02-20 18:33:47 +0900

    feat: chat hook & modal
    Co-authored-by: ojj1123 <rojay.developer@gmail.com>

--- frontend/src/providers/chat-modal-provider.tsx
+++ frontend/src/providers/chat-modal-provider.tsx
@@ -1,13 +1,54 @@
-import {createContext, type PropsWithChildren, useContext} from 'react'
+import {
+  createContext,
+  type PropsWithChildren,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from 'react'
 
-import {useModal, type UseModalReturn} from '@/hooks/use-modal'
+import {useModal} from '@/hooks/use-modal'
 
-const ChatModalContext = createContext<UseModalReturn | null>(null)
+export type ChatModalOptions = {content: 'HOME'} | {content: 'ROOM'; id: number}
+export interface ChatModalContextType {
+  open: boolean
+  handleOpen: (options: ChatModalOptions) => void
+  handleClose: () => void
+  options: ChatModalOptions
+}
 
+const ChatModalContext = createContext<ChatModalContextType | null>(null)
+
+const initialOptions: ChatModalOptions = {
+  content: 'HOME',
+}
 export function ChatModalProvider({children}: PropsWithChildren) {
   const modal = useModal()
+  const [options, setOptions] = useState<ChatModalOptions>(initialOptions)
 
-  return <ChatModalContext.Provider value={modal}>{children}</ChatModalContext.Provider>
+  const handleOpen = useCallback(
+    (options: ChatModalOptions) => {
+      modal.onOpen()
+      setOptions(options)
+    },
+    [modal],
+  )
+
+  const handleClose = useCallback(() => {
+    modal.onClose()
+    setOptions(initialOptions)
+  }, [modal])
+
+  const context = useMemo(
+    () => ({
+      open: modal.open,
+      handleOpen,
+      handleClose,
+      options,
+    }),
+    [options, modal, handleClose, handleOpen],
+  )
+  return <ChatModalContext.Provider value={context}>{children}</ChatModalContext.Provider>
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
