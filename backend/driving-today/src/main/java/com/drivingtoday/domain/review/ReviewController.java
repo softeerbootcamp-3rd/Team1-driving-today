b4a3ced3c7f513260bbb0d76948bdf9d74128796
서지원 <readyjiwon@gmail.com>
2024-02-20 17:19:11 +0900

    feat : api refactoring (#198)

--- backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewController.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/review/ReviewController.java
@@ -1,39 +1,38 @@
 package com.drivingtoday.domain.review;
 
 
-import com.drivingtoday.domain.review.dto.ReviewFindRequest;
-import com.drivingtoday.domain.review.dto.ReviewInfo;
 import com.drivingtoday.domain.review.dto.ReviewRequest;
 import com.drivingtoday.global.auth.config.JwtFilter;
 import com.drivingtoday.global.auth.constants.Authentication;
+import com.drivingtoday.global.dto.SliceResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class ReviewController {
+
     private final ReviewFindService reviewFindService;
     private final ReviewAddService reviewAddService;
 
-    @Operation(summary = "특정 강사에 대한 리뷰들 조회 API")
-    @GetMapping("/student/reviews")
-    public ResponseEntity<List<ReviewInfo>> reviewList(@Valid @ModelAttribute ReviewFindRequest request) {
-        log.info("dto: {}", request.getInstructorId());
-        List<ReviewInfo> reviews = reviewFindService.findReviews(request);
+    @Operation(summary = "[학생] 특정 강사에 대한 리뷰들 조회 API")
+    @GetMapping("/reviews")
+    public ResponseEntity<SliceResponse<Review>> reviewList(@RequestParam("instructorId") Long instructorId,
+                                                            @RequestParam("pageNumber") Integer pageNumber,
+                                                            @RequestParam("pageSize") Integer pageSize) {
+        SliceResponse<Review> reviews = reviewFindService.findReviews(instructorId, pageNumber, pageSize);
         return ResponseEntity.ok(reviews);
     }
-    
-    @Operation(summary = "리뷰 등록하기 API")
-    @PostMapping("/student/review")
-    public ResponseEntity<Void> reviewAdd(@RequestBody @Valid ReviewRequest reviewRequest) {
+
+    @Operation(summary = "[학생] 리뷰 등록하기 API")
+    @PostMapping("/review")
+    public ResponseEntity<Void> reviewAdd(@RequestBody ReviewRequest reviewRequest) {
         Authentication authentication = JwtFilter.getAuthentication();
         Long newReviewId = reviewAddService.addReview(reviewRequest, authentication.getId());
         return ResponseEntity.created(URI.create("/reviews/" + newReviewId)).build();
--- backend/driving-today/src/main/java/com/drivingtoday/global/user/dto/StudentRegisterRequest.java
+++ backend/driving-today/src/main/java/com/drivingtoday/global/user/dto/StudentRegisterRequest.java
@@ -3,13 +3,13 @@ package com.drivingtoday.global.user.dto;
 import com.drivingtoday.domain.student.Student;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class StudentJoinRequest {
+public class StudentRegisterRequest {
+
     @NotNull
     private String name;
 
