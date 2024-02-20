1a2dbaf303d4cce01735417e336784d73c0fb095
seojeewon <readyjiwon@gmail.com>
2024-02-20 16:06:47 +0900

    [BE] refactor : reservation api refactoring

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationController.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationController.java
@@ -21,46 +21,43 @@ public class ReservationController {
     private final ReservationListService reservationListService;
     private final ReservationDeleteService reservationDeleteService;
 
-    @Operation(summary = "예약만들기 API")
-    @PostMapping("/student/reservations")
+    @Operation(summary = "[학생] 예약만들기 API")
+    @PostMapping("/reservation")
     public ResponseEntity<Void> reservationAdd(@RequestBody ReservationRequest reservationRequest) {
         Long studentId = JwtFilter.getAuthentication().getId();
         Long newReservationId = reservationAddService.addReservation(reservationRequest, studentId);
         return ResponseEntity.created(URI.create("/reservation/" + newReservationId)).build();
     }
 
-    @Operation(summary = "학생이 본인 예약리스트 확인하기 API")
-    @GetMapping("/student/reservations")
-    public ResponseEntity<List<ReservationStudentResponse>> reservationList(@RequestParam("isUpcoming") Boolean isUpcoming) {
+    @Operation(summary = "[학생] 본인 예약리스트 조회 API")
+    @GetMapping("/reservations/student")
+    public ResponseEntity<List<ReservationStudentResponse>> reservationList(@RequestParam("status") String status) {
         Long studentId = JwtFilter.getAuthentication().getId();
         List<ReservationStudentResponse> allStudentReservation =
-                reservationListService.findAllStudentReservation(studentId, isUpcoming);
+                reservationListService.findAllStudentReservation(studentId, status);
         return ResponseEntity.ok(allStudentReservation);
     }
 
 
-    @Operation(summary = "강사가 본인 예약리스트 확인하기 API")
-    @GetMapping("/instructor/reservations")
+    @Operation(summary = "[강사] 본인 예약리스트 조회 API")
+    @GetMapping("/reservations/instructor")
     public ResponseEntity<List<ReservationInstructorResponse>> instructorReservationList(@RequestParam("pageNumber") Integer pageNumber,
                                                                                          @RequestParam("pageSize") Integer pageSize,
-                                                                                         @RequestParam("isUpcoming") Boolean isUpcoming) {
+                                                                                         @RequestParam("status") String status) {
         Long studentId = JwtFilter.getAuthentication().getId();
         List<ReservationInstructorResponse> allInstructorReservation =
-                reservationListService.findAllInstructorReservation(studentId, pageNumber, pageSize, isUpcoming);
+                reservationListService.findAllInstructorReservation(studentId, pageNumber, pageSize, status);
         return ResponseEntity.ok(allInstructorReservation);
     }
 
-    @Operation(summary = "학생이 예약 취소하기 API")
-    @DeleteMapping("/student/reservations/{reservation_id}")
-    public ResponseEntity<Void> cancelStudentReservation(@PathVariable("reservation_id") Long reservationId) {
-        reservationDeleteService.cancelStudentReservation(reservationId, JwtFilter.getAuthentication().getId());
-        return ResponseEntity.ok().build();
-    }
-
-    @Operation(summary = "강사가 예약 거절하기 API")
-    @DeleteMapping("/instructor/reservations/{reservation_id}")
-    public ResponseEntity<Void> rejectInstructorReservation(@PathVariable("reservation_id") Long reservationId) {
-        reservationDeleteService.rejectInstructorReservation(reservationId, JwtFilter.getAuthentication().getId());
+    @Operation(summary = "[공통] 예약 취소하기 API")
+    @DeleteMapping("/reservations/{reservation_id}")
+    public ResponseEntity<Void> cancelStudentReservation(@PathVariable("reservation_id") Long reservationId, @RequestParam("role") String role) {
+        if (role.equals("student")) {
+            reservationDeleteService.cancelStudentReservation(reservationId, JwtFilter.getAuthentication().getId());
+        } else {
+            reservationDeleteService.rejectInstructorReservation(reservationId, JwtFilter.getAuthentication().getId());
+        }
         return ResponseEntity.ok().build();
     }
 
