6672bf59f0e25664356b4b7be2959d528e897f75
Junhea Hong <97426534+junhea@users.noreply.github.com>
2024-02-21 21:46:55 +0900

    [FE] feat: save session to browser (#177)

--- frontend/src/utils/session.ts
+++ frontend/src/utils/session.ts
@@ -15,7 +15,24 @@ export interface SessionProvider {
   getAccessToken: () => string
 }
 
+const SESSION_KEY = 'session'
+
+function getSessionFromStorage() {
+  const sessionStr = localStorage.getItem(SESSION_KEY)
+  if (!sessionStr) return
+  return JSON.parse(sessionStr) as Session
+}
+
+function saveSessionToStorage(session: Session) {
+  localStorage.setItem(SESSION_KEY, JSON.stringify(session))
+}
+
+function resetSession() {
+  localStorage.removeItem(SESSION_KEY)
+}
+
 export const sessionProvider: SessionProvider = {
+  session: getSessionFromStorage(),
   async login(role: UserRole, email: string, password: string) {
     const body = JSON.stringify({
       email,
@@ -35,11 +52,15 @@ export const sessionProvider: SessionProvider = {
 
     const sessionResponse = (await res.json()) as LoginResponse
 
-    return {
+    const session = {
       id: sessionResponse.id,
       role: role,
       accessToken: sessionResponse.jwt.accessToken,
-    }
+    } as Session
+
+    saveSessionToStorage(session)
+
+    return session
   },
   logout: () => {},
   getAccessToken() {
