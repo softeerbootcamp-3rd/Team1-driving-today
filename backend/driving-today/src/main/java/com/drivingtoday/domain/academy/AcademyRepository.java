c8db0a59d26a78251c72dd1bbf1f18498a6cdb11
서지원 <readyjiwon@gmail.com>
2024-02-22 00:03:16 +0900

    [BE] feat : 학원 검색 api  (#226)

--- backend/driving-today/src/main/java/com/drivingtoday/domain/academy/AcademyRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/academy/AcademyRepository.java
@@ -2,5 +2,8 @@ package com.drivingtoday.domain.academy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AcademyRepository extends JpaRepository<Academy, Long> {
+    List<Academy> findAllByName(String name);
 }
