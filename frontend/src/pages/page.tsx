e6320cb70dd24e2a3681cac9f0c36fce5a10ae65
junhea <97426534+junhea@users.noreply.github.com>
2024-02-22 23:18:54 +0900

    feat: style header

--- frontend/src/pages/page.tsx
+++ frontend/src/pages/page.tsx
@@ -7,11 +7,20 @@ import {Landing} from './components/landing'
 export function LandingPage() {
   return (
     <>
-      <Header>
+      <Landing />
+      <Header
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          backdropFilter: 'blur(8px)',
+          border: 'none',
+          background: 'rgba(0,0,0,0)',
+        }}
+      >
         <Link to="/login">로그인</Link>
         <Link to="/register">회원가입</Link>
       </Header>
-      <Landing />
       <Outlet />
     </>
   )
