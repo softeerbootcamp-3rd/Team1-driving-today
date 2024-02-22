6f92d319dcedaf81b05839f88699ebfbe9fcb907
Jeongjin Oh <rojay.developer@gmail.com>
2024-02-22 23:52:47 +0900

    refactor: styled component 분리

--- frontend/src/pages/components/chat-modal/chat-room/instructor-chat-room.tsx
+++ frontend/src/pages/components/chat-modal/chat-room/instructor-chat-room.tsx
@@ -1,5 +1,3 @@
-import {Theme} from '@emotion/react'
-import styled from '@emotion/styled'
 import {Fragment, Suspense, useCallback, useEffect, useRef} from 'react'
 
 import {Flex} from '@/components/flex'
@@ -15,6 +13,7 @@ import {sessionProvider} from '@/utils/session'
 import {isDifferenceOneDay, timestampToHHMM} from '@/utils/time'
 
 import {ChatRoomLayout} from './chat-room-layout'
+import {Avartar, ChatInput, ChatList, ChatMessage, ChatTime, SendButton, TimeStemp} from './styles'
 
 interface InstructorChatRoomProps {
   instructorId: number
@@ -225,70 +224,3 @@ function InstructorChatList({
     </ChatList>
   )
 }
-
-const TimeStemp = styled.li(({theme}) => ({
-  display: 'flex',
-  alignItems: 'center',
-  '& > div': {
-    borderRadius: '9999px',
-    textAlign: 'center',
-    border: `1px solid ${theme.color.gray300}`,
-    backgroundColor: theme.color.white,
-    color: theme.color.gray900,
-    fontSize: '1.4rem',
-    padding: '0.5rem 1rem',
-    flex: '0.8 1 0',
-  },
-  '&:before, &:after': {
-    content: '""',
-    width: '100%',
-    height: '1px',
-    backgroundColor: theme.color.gray300,
-    flex: '1 1 0',
-  },
-}))
-
-const ChatList = styled.ul(() => ({
-  display: 'flex',
-  flexDirection: 'column',
-  width: '100%',
-  height: '100%',
-  paddingBottom: '2rem',
-  gap: '1rem',
-}))
-
-const Avartar = styled.img(({width, height}) => ({
-  borderRadius: '50%',
-  width,
-  height,
-}))
-
-const ChatMessage = styled.div<{bgColor: keyof Theme['color']; color: keyof Theme['color']}>(
-  ({theme, bgColor, color}) => ({
-    padding: '1rem',
-    fontSize: '1.4rem',
-    backgroundColor: theme.color[bgColor],
-    color: color,
-    borderRadius: '0.8rem',
-  }),
-)
-
-const ChatTime = styled.span(({theme}) => ({
-  fontSize: '1.2rem',
-  color: theme.color.gray700,
-  alignSelf: 'flex-end',
-}))
-
-const ChatInput = styled.input(({theme}) => ({
-  backgroundColor: theme.color.gray200,
-  flex: '1 1 0',
-  borderRadius: '99px',
-  fontSize: '1.4rem',
-  padding: '0.7rem 2rem',
-}))
-
-const SendButton = styled.button(({theme}) => ({
-  borderRadius: '50%',
-  backgroundColor: theme.color.primary,
-  padding: '1rem',
-}))
---
+++ frontend/src/pages/components/chat-modal/chat-room/styles.tsx
@@ -0,0 +1,69 @@
+import {type Theme} from '@emotion/react'
+import styled from '@emotion/styled'
+
+export const TimeStemp = styled.li(({theme}) => ({
+  display: 'flex',
+  alignItems: 'center',
+  '& > div': {
+    borderRadius: '9999px',
+    textAlign: 'center',
+    border: `1px solid ${theme.color.gray300}`,
+    backgroundColor: theme.color.white,
+    color: theme.color.gray900,
+    fontSize: '1.4rem',
+    padding: '0.5rem 1rem',
+    flex: '0.8 1 0',
+  },
+  '&:before, &:after': {
+    content: '""',
+    width: '100%',
+    height: '1px',
+    backgroundColor: theme.color.gray300,
+    flex: '1 1 0',
+  },
+}))
+
+export const ChatList = styled.ul(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  height: '100%',
+  paddingBottom: '2rem',
+  gap: '1rem',
+}))
+
+export const Avartar = styled.img(({width, height}) => ({
+  borderRadius: '50%',
+  width,
+  height,
+}))
+
+export const ChatMessage = styled.div<{bgColor: keyof Theme['color']; color: keyof Theme['color']}>(
+  ({theme, bgColor, color}) => ({
+    padding: '1rem',
+    fontSize: '1.4rem',
+    backgroundColor: theme.color[bgColor],
+    color: color,
+    borderRadius: '0.8rem',
+  }),
+)
+
+export const ChatTime = styled.span(({theme}) => ({
+  fontSize: '1.2rem',
+  color: theme.color.gray700,
+  alignSelf: 'flex-end',
+}))
+
+export const ChatInput = styled.input(({theme}) => ({
+  backgroundColor: theme.color.gray200,
+  flex: '1 1 0',
+  borderRadius: '99px',
+  fontSize: '1.4rem',
+  padding: '0.7rem 2rem',
+}))
+
+export const SendButton = styled.button(({theme}) => ({
+  borderRadius: '50%',
+  backgroundColor: theme.color.primary,
+  padding: '1rem',
+}))
