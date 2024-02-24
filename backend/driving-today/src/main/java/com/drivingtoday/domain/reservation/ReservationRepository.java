5d9d86ba649d4c6bc9d7d396ab9a0ba304e90fae
서지원 <readyjiwon@gmail.com>
2024-02-24 15:59:09 +0900

    [BE] feat : reservation fetch join 적용 (#264)

--- backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/reservation/ReservationRepository.java
@@ -2,43 +2,54 @@ package com.drivingtoday.domain.reservation;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     List<Reservation> findAllByInstructorId(Long instructorId);
 
-    @Query("SELECT r FROM Reservation r WHERE r.isAccepted = true AND (r.reservationDate > :currentDate " +
+    @Query("SELECT r FROM Reservation r " +
+            "JOIN FETCH r.instructor i JOIN FETCH i.academy a " +
+            "WHERE r.isAccepted = true AND (r.reservationDate > :currentDate " +
             "OR (r.reservationDate = :currentDate AND r.reservationTime > :currentTime)) " +
             "AND r.student.id = :studentId")
     List<Reservation> findFutureReservationsByStudent
             (Long studentId, LocalDate currentDate, Integer currentTime);
 
 
-    @Query("SELECT r FROM Reservation r WHERE r.isAccepted = true AND (r.reservationDate < :currentDate " +
+    @Query("SELECT r FROM Reservation r " +
+            "JOIN FETCH r.instructor i JOIN FETCH i.academy a " +
+            "WHERE r.isAccepted = true AND (r.reservationDate < :currentDate " +
             "OR (r.reservationDate = :currentDate AND r.reservationTime <= :currentTime)) " +
             "AND r.student.id = :studentId")
     List<Reservation> findPastReservationsByStudent
             (Long studentId, LocalDate currentDate, Integer currentTime);
 
-    @Query("SELECT r FROM Reservation r WHERE r.isAccepted = true AND (r.reservationDate > :currentDate " +
+    @Query("SELECT r FROM Reservation r " +
+            "JOIN FETCH r.student s " +
+            "WHERE r.isAccepted = true AND (r.reservationDate > :currentDate " +
             "OR (r.reservationDate = :currentDate AND r.reservationTime > :currentTime)) " +
             "AND r.instructor.id = :instructorId")
     Page<Reservation> findFutureReservationsByInstructor
             (Long instructorId, LocalDate currentDate, Integer currentTime, Pageable pageable);
 
 
-    @Query("SELECT r FROM Reservation r WHERE r.isAccepted = true AND (r.reservationDate < :currentDate " +
+    @Query("SELECT r FROM Reservation r " +
+            "JOIN FETCH r.student s " +
+            "WHERE r.isAccepted = true AND (r.reservationDate < :currentDate " +
             "OR (r.reservationDate = :currentDate AND r.reservationTime <= :currentTime)) " +
             "AND r.instructor.id = :instructorId")
     Page<Reservation> findPastReservationsByInstructor
             (Long instructorId, LocalDate currentDate, Integer currentTime, Pageable pageable);
 
-    @Query("SELECT COUNT(r) FROM Reservation r WHERE r.isAccepted = true AND r.instructor.id = :instructorId AND r.reservationDate = :date " +
+    @Query("SELECT COUNT(r) FROM Reservation r " +
+            "WHERE r.isAccepted = true AND r.instructor.id = :instructorId AND r.reservationDate = :date " +
             "AND ((r.reservationTime BETWEEN :startTime AND :startTime + :trainingTime - 1) " +
             "OR (r.reservationTime + r.trainingTime - 1 BETWEEN :startTime AND :startTime + :trainingTime - 1))" )
     Integer countConfilctTimeSlot(Long instructorId, int startTime, int trainingTime, LocalDate date);
