992602ae6ece7783803fc857f77540f1e7923af4
jsLeen <lejs1008@naver.com>
2024-02-24 21:52:21 +0900

    [BE] feat : 리뷰 리스트 응답 형식 변경 (#274)

--- backend/driving-today/src/test/java/com/drivingtoday/domain/reservation/ReservationListServiceFetchJoinTest.java
+++ backend/driving-today/src/test/java/com/drivingtoday/domain/reservation/ReservationListServiceFetchJoinTest.java
@@ -18,23 +18,23 @@ class ReservationListServiceFetchJoinTest {
     @Autowired
     ReservationListService listService;
 
-    @Test
-    @DisplayName("fetch join을 하기 전, Lazy Loading으로 연관관계 호출할 때 걸리는 시간")
-    void before_fetch_join(){
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-        List<ReservationStudentResponse> reservations = listService.findAllStudentReservation1(1L, "scheduled");
-        stopWatch.stop();
-        log.info(stopWatch.getTotalTimeMillis() + "ms");
-    }
+//    @Test
+//    @DisplayName("fetch join을 하기 전, Lazy Loading으로 연관관계 호출할 때 걸리는 시간")
+//    void before_fetch_join(){
+//        StopWatch stopWatch = new StopWatch();
+//        stopWatch.start();
+//        List<ReservationStudentResponse> reservations = listService.findAllStudentReservation1(1L, "scheduled");
+//        stopWatch.stop();
+//        log.info(stopWatch.getTotalTimeMillis() + "ms");
+//    }
 
-    @Test
-    @DisplayName("fetch join 후, 걸리는 시간")
-    void after_fetch_join(){
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-        List<ReservationStudentResponse> reservations = listService.findAllStudentReservation2(1L, "scheduled");
-        stopWatch.stop();
-        log.info(stopWatch.getTotalTimeMillis() + "ms");
-    }
+//    @Test
+//    @DisplayName("fetch join 후, 걸리는 시간")
+//    void after_fetch_join(){
+//        StopWatch stopWatch = new StopWatch();
+//        stopWatch.start();
+//        List<ReservationStudentResponse> reservations = listService.findAllStudentReservation2(1L, "scheduled");
+//        stopWatch.stop();
+//        log.info(stopWatch.getTotalTimeMillis() + "ms");
+//    }
 }
