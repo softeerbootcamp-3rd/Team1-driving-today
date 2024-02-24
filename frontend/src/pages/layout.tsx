2a4c067d9a90b4073adfdc95fa688e18a1ff0cc8
Jeongjin Oh <rojay.developer@gmail.com>
2024-02-24 20:21:16 +0900

    feat: root error boundary

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,13 +1,15 @@
 import styled from '@emotion/styled'
-import {Outlet, useLoaderData} from 'react-router-dom'
+import {Link, Outlet, useLoaderData} from 'react-router-dom'
 
+import {Flex} from '@/components/flex'
 import {Sidebar} from '@/components/sidebar'
+import {Typography} from '@/components/typography'
 import {useChatModal} from '@/providers'
 
 import {UserRole} from '../utils/session'
 import {ChatModalContainer} from './components'
 
-export function Layout() {
+function LayoutRoot() {
   const role = useLoaderData() as UserRole
 
   return (
@@ -23,6 +25,52 @@ const LayoutContainer = styled.main(() => ({
   height: '100%',
   width: '100%',
 }))
+
+function LayoutErrorBoundary() {
+  return (
+    <Flex
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap="2rem"
+      style={{width: '100%', height: '100%'}}
+    >
+      <Typography size="1.4rem" weight="normal" color="gray900">
+        알 수 없는 에러입니다.
+      </Typography>
+      <BackHomeButton to="/" replace>
+        홈으로 돌아가기
+      </BackHomeButton>
+      <img src="/error.png" width="200" height="200" />
+    </Flex>
+  )
+}
+
+const BackHomeButton = styled(Link)(({theme}) => ({
+  display: 'inline-block',
+  border: `2px solid ${theme.color.gray900}`,
+  color: '#fff',
+  textTransform: 'uppercase',
+  fontWeight: 600,
+  padding: '0.75rem 1rem 0.6rem',
+  transition: 'all 0.2s linear',
+  boxShadow: '0 15px 15px -11px rgba(0,0,0, 0.4)',
+  background: theme.color.gray900,
+  borderRadius: '6px',
+  fontSize: '1.6rem',
+  width: 'fit-content',
+  marginTop: '2rem',
+
+  '&:hover': {
+    background: theme.color.gray900,
+    color: theme.color.gray300,
+    transform: 'scale(1.1)',
+  },
+}))
+
+export const Layout = Object.assign(LayoutRoot, {
+  ErrorBoundary: LayoutErrorBoundary,
+})
 
 function StudentSidebar() {
   const {handleOpen} = useChatModal()
