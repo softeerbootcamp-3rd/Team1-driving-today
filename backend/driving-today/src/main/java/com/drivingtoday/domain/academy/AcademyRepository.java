6fcc7a53a0a6406afe3a4918cb4d8c55be6da9c3
서지원 <readyjiwon@gmail.com>
2024-02-24 11:50:55 +0900

    [BE] feat : 이름으로 학원 찾는 쿼리 include 방식으로 변경 (#257)

--- backend/driving-today/src/main/java/com/drivingtoday/domain/academy/AcademyRepository.java
+++ backend/driving-today/src/main/java/com/drivingtoday/domain/academy/AcademyRepository.java
@@ -1,9 +1,11 @@
 package com.drivingtoday.domain.academy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface AcademyRepository extends JpaRepository<Academy, Long> {
+    @Query("Select a from Academy a WHERE a.name like %:name%")
     List<Academy> findAllByName(String name);
 }
